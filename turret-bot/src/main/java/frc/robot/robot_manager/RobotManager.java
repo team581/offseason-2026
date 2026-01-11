4072830c8867f653acddc07c11b6c7351ad25844
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2026-01-11 14:40:08 -0800

    start robot manager turret code

--- turret-bot/src/main/java/frc/robot/robot_manager/RobotManager.java
+++ turret-bot/src/main/java/frc/robot/robot_manager/RobotManager.java
@@ -8,9 +8,11 @@ import frc.robot.util.scheduling.SubsystemPriority;
 public class RobotManager extends StateMachineSubsystem<RobotState> {
   public final LocalizationSubsystem localization;
   public final SwerveSubsystem swerve;
+//  public final TurretSubsystem turret;
 
   public RobotManager(LocalizationSubsystem localization, SwerveSubsystem swerve) {
-    super(SubsystemPriority.ROBOT_MANAGER, RobotState.PLACEHOLDER_STATE);
+    super(SubsystemPriority.ROBOT_MANAGER, RobotState.IDLE);
+//    this.turret = turret;
     this.localization = localization;
     this.swerve = swerve;
   }
@@ -18,7 +20,9 @@ public class RobotManager extends StateMachineSubsystem<RobotState> {
   @Override
   protected void afterTransition(RobotState newState) {
     switch (newState) {
-      case PLACEHOLDER_STATE -> swerve.normalDriveRequest();
+      case AIMING -> swerve.normalDriveRequest();
+      case IDLE -> swerve.normalDriveRequest();
+      case LOCKED -> swerve.normalDriveRequest();
     }
   }
 }
