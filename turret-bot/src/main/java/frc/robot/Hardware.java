797e82478fe95bc653396bcdd1f28476061f6ee8
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2026-01-11 16:27:49 -0800

    update turret motor id

--- turret-bot/src/main/java/frc/robot/Hardware.java
+++ turret-bot/src/main/java/frc/robot/Hardware.java
@@ -1,5 +1,7 @@
 package frc.robot;
 
+import com.ctre.phoenix6.hardware.TalonFX;
+
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.generated.RobotTunerConstants;
 import frc.robot.generated.RobotTunerConstants.TunerSwerveDrivetrain;
@@ -7,6 +9,8 @@ import frc.robot.generated.RobotTunerConstants.TunerSwerveDrivetrain;
 public class Hardware {
   public final XboxController driverController = new XboxController(0);
   public final XboxController operatorController = new XboxController(1);
+
+  public final TalonFX turretMotor = new TalonFX(15);
 
   public final TunerSwerveDrivetrain drivetrain =
       new TunerSwerveDrivetrain(
