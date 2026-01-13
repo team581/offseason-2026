d34b68cafea6727fd72724b948d553c32105d027
Owen <117956892+Owen756@users.noreply.github.com>
2026-01-13 15:49:26 -0800

    start Hopper for dumper bot

---
+++ dumper-bot/src/main/java/frc/robot/hopper/HopperState.java
@@ -0,0 +1,23 @@
+package frc.robot.hopper;
+
+import frc.robot.hopper.HopperState;
+
+public enum HopperState {
+   UNTUNED(0.0),
+  UNJAM(UNTUNED),
+
+  STOPPED(UNTUNED),
+  IDLE(UNTUNED),
+  INTAKING(UNTUNED),
+  OUTTAKING(UNTUNED);
+
+  public final double volts;
+
+  private HopperState(HopperState state) {
+    this.volts = state.volts;
+  }
+
+  private HopperState(double volts) {
+    this.volts = volts;
+  }
+}
