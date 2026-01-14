04094b14bb4d8acc6c75e00009191fc80db4ac38
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2026-01-13 17:03:37 -0800

    add turret subsystem to robot manager

--- turret-bot/src/main/java/frc/robot/robot_manager/RobotManager.java
+++ turret-bot/src/main/java/frc/robot/robot_manager/RobotManager.java
@@ -15,24 +15,30 @@ public class RobotManager extends StateMachineSubsystem<RobotState> {
   public final TurretSubsystem turret;
   public final VisionSubsystem vision;
 
-  public RobotManager(
-      LocalizationSubsystem localization,
-      SwerveSubsystem swerve,
-      TurretSubsystem turret,
-      VisionSubsystem vision) {
+  public RobotManager(LocalizationSubsystem localization, SwerveSubsystem swerve, TurretSubsystem turret, VisionSubsystem vision) {
     super(SubsystemPriority.ROBOT_MANAGER, RobotState.IDLE);
+    this.turret = turret;
     this.localization = localization;
     this.swerve = swerve;
     this.vision = vision;
-    this.turret = turret;
   }
-
+    @Override
+    protected RobotState getNextState(RobotState currentState) {
+        return switch (currentState) {
+            default -> currentState;
+        };
+    }
   @Override
   protected void afterTransition(RobotState newState) {
     switch (newState) {
-      case AIMING -> swerve.normalDriveRequest();
-      case IDLE -> swerve.normalDriveRequest();
-      case LOCKED -> swerve.normalDriveRequest();
+      case AUTO_AIM ->
+      swerve.normalDriveRequest();
+      case MANUAL_AIM -> 
+      swerve.normalDriveRequest();
+      case IDLE -> 
+      swerve.normalDriveRequest();
+      case LOCKED -> 
+      swerve.normalDriveRequest();
     }
   }
 
