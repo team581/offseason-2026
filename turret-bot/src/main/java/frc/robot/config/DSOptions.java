b3feeb9f83ef29d11350e01049e860160fa3a276
Simon <87281874+simonp17@users.noreply.github.com>
2026-01-15 11:30:01 -0800

    Add ds option for turret angle comp vision

--- turret-bot/src/main/java/frc/robot/config/DSOptions.java
+++ turret-bot/src/main/java/frc/robot/config/DSOptions.java
@@ -5,10 +5,9 @@ import edu.wpi.first.networktables.BooleanSubscriber;
 
 public final class DSOptions {
   public static final BooleanSubscriber SENSOR_BROKEN = DSOption.of("SensorBroken", false);
-  public static final BooleanSubscriber VISION_CAMERA_POSITION_COMPENSATION =
-      DSOption.of("CameraPositionComp", true);
-  public static final BooleanSubscriber VISION_TURRET_POSITION_COMPENSATION =
-      DSOption.of("TurretPositionComp", true);
+  public static final BooleanSubscriber VISION_CAMERA_POSITION_COMPENSATION = DSOption.of("CameraPositionComp", true);
+  public static final BooleanSubscriber VISION_TURRET_POSITION_COMPENSATION = DSOption.of("TurretPositionComp", true);
+  public static final BooleanSubscriber VISION_TURRET_ANGLE_COMPENSATION = DSOption.of("TurretAngleComp", true);
 
   private DSOptions() {}
 }
