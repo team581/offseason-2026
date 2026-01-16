9a057aad36194575a0c7528a09a4991713cf952c
Owen <117956892+Owen756@users.noreply.github.com>
2026-01-15 16:51:00 -0800

    add hopper to robot manager

--- dumper-bot/src/main/java/frc/robot/Robot.java
+++ dumper-bot/src/main/java/frc/robot/Robot.java
@@ -10,6 +10,7 @@ import com.team581.trailblazer.trackers.HeuristicPathTracker;
 import dev.doglog.DogLog;
 import edu.wpi.first.math.controller.PIDController;
 import frc.robot.generated.BuildConstants;
+import frc.robot.hopper.Hopper;
 import frc.robot.intake.Intake;
 import frc.robot.robot_manager.RobotManager;
 import frc.robot.shooter.Shooter;
@@ -21,20 +22,11 @@ public class Robot extends Base581Robot {
       new Trailblazer(
           new HeuristicPathTracker(new PoseErrorTolerance(0.5, 10)),
           new PidPathFollower(new PIDController(3.5, 0, 0), new PIDController(4.0, 0, 0)));
-<<<<<<< HEAD
-  private final SwerveSubsystem swerve = new SwerveSubsystem(hardware.drivetrain, trailblazer);
-  private final IntakeSubsystem intake = new IntakeSubsystem(hardware.intakeMotor);
-  private final HopperSubsystem hopper =
-      new HopperSubsystem(hardware.leftHopperMotor, hardware.rightHopperMotor);
-  private final ShooterSubsystem shooter =
-      new ShooterSubsystem(hardware.leftShooterMotor, hardware.rightShooterMotor);
-=======
   private final Swerve swerve = new Swerve(hardware.drivetrain, trailblazer);
   private final Intake intake = new Intake(hardware.intakeMotor);
-
+  private final Hopper hopper = new Hopper(hardware.leftHopperMotor, hardware.rightHopperMotor);
   private final Shooter shooter =
       new Shooter(hardware.leftShooterMotor, hardware.rightShooterMotor);
->>>>>>> 96e8e1e50b71d0309f7f8cfeb60a1c81e5381009
   private final RobotManager robotManager = new RobotManager(intake, shooter);
 
   @SuppressWarnings("unused") // Registers itself as a
