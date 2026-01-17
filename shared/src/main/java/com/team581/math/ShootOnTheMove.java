4ff5d1049d62bdd92ee29c5a4a880815fc53f04e
Jordan <117786700+jordanjcoderman@users.noreply.github.com>
2026-01-17 11:11:57 -0800

    Fix error in shoot on the move equation

--- shared/src/main/java/com/team581/math/ShootOnTheMove.java
+++ shared/src/main/java/com/team581/math/ShootOnTheMove.java
@@ -7,10 +7,10 @@ import edu.wpi.first.math.kinematics.ChassisSpeeds;
 public class ShootOnTheMove {
   public static Pose2d getVelocityCompensatedGoal(
       Pose2d target, ChassisSpeeds robotVelocity, double timeOfFlight) {
-    // Compensated goal = real goal + (robot velocity * time of flight of ball)
+    // Compensated goal = real goal - (robot velocity * time of flight of ball)
     return new Pose2d(
-        target.getX() + (robotVelocity.vxMetersPerSecond * timeOfFlight),
-        target.getY() + (robotVelocity.vyMetersPerSecond * timeOfFlight),
+        target.getX() - (robotVelocity.vxMetersPerSecond * timeOfFlight),
+        target.getY() - (robotVelocity.vyMetersPerSecond * timeOfFlight),
         Rotation2d.kZero);
   }
 
