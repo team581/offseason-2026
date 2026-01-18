87a152c1039b3ae21f174eda17e8904a6f2f397e
Simon <87281874+simonp17@users.noreply.github.com>
2026-01-18 14:45:53 -0800

    Change control request back to Position Voltage

--- turret-bot/src/main/java/frc/robot/turret/Turret.java
+++ turret-bot/src/main/java/frc/robot/turret/Turret.java
@@ -6,6 +6,7 @@ import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
+import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import com.team581.simkit.SimKit;
@@ -40,8 +41,8 @@ public class Turret extends StateMachineSubsystem<TurretState> {
   private final LinearFilter currentFilter = LinearFilter.movingAverage(7);
   private double rawCurrent = 0.0;
   private double filteredCurrent = 0.0;
-  private final MotionMagicVoltage positionRequest =
-      new MotionMagicVoltage(0.0).withEnableFOC(false);
+  private final PositionVoltage positionRequest =
+      new PositionVoltage(0.0).withEnableFOC(false);
 
   public Turret(TalonFX motor) {
     super(SubsystemPriority.TURRET, TurretState.UNHOMED);
