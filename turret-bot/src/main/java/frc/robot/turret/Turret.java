5c45a52efbbea75b74a788bca634fcf7c51f0d00
Simon <87281874+simonp17@users.noreply.github.com>
2026-01-19 10:31:34 -0800

    Latency compensate turret angle Fixes #83

--- turret-bot/src/main/java/frc/robot/turret/Turret.java
+++ turret-bot/src/main/java/frc/robot/turret/Turret.java
@@ -1,5 +1,6 @@
 package frc.robot.turret;
 
+import com.ctre.phoenix6.BaseStatusSignal;
 import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
 import com.ctre.phoenix6.configs.FeedbackConfigs;
 import com.ctre.phoenix6.configs.MotorOutputConfigs;
@@ -89,13 +90,12 @@ public class Turret extends StateMachineSubsystem<TurretState> {
     DogLog.log("Turret/Angle", currentAngle);
 
     // Predict the turret's current angle to account for sensor latency
-    double velocityDegreesPerSecond =
-        Units.rotationsToDegrees(motor.getVelocity().getValueAsDouble());
-    double predictedAngle = currentAngle + (velocityDegreesPerSecond * LATENCY_SECONDS.get());
-    DogLog.log("Turret/PredictedAngle", predictedAngle);
+    double latencyCompensatedAngle = Units.rotationsToDegrees(BaseStatusSignal.getLatencyCompensatedValueAsDouble(
+        motor.getPosition(), motor.getVelocity()));
+    DogLog.log("Turret/LatencyCompensatedAngle", latencyCompensatedAngle);
 
     // Add the predicted angle to the vision buffer at the current timestamp
-    vision.addTurretObservation(Timer.getFPGATimestamp(), Rotation2d.fromDegrees(predictedAngle));
+    vision.addTurretObservation(Timer.getFPGATimestamp(), Rotation2d.fromDegrees(latencyCompensatedAngle));
   }
 
   @Override
