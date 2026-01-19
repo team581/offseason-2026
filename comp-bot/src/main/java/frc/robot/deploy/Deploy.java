15acb0ca565ab259bb65c1329339de9344b0e19a
Simon <87281874+simonp17@users.noreply.github.com>
2026-01-19 10:45:11 -0800

    Assign student for deploy homing angle

--- comp-bot/src/main/java/frc/robot/deploy/Deploy.java
+++ comp-bot/src/main/java/frc/robot/deploy/Deploy.java
@@ -10,6 +10,7 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import com.team581.util.state_machines.StateMachineSubsystem;
 import com.team581.util.tuning.TunablePid;
+
 import edu.wpi.first.math.util.Units;
 import frc.robot.util.scheduling.SubsystemPriority;
 
@@ -63,7 +64,7 @@ public class Deploy extends StateMachineSubsystem<DeployState> {
     return switch (currentState) {
       case HOMING -> {
         if (motor.getStatorCurrent().getValueAsDouble() > 20) {
-          // TODO: reset the encoder to a homed position (this is some angle we dont know yet)
+          // TODO(@Anju501): reset the encoder to a homed position (this is some angle we dont know yet)
           yield DeployState.STOWED;
         } else {
           yield currentState;
