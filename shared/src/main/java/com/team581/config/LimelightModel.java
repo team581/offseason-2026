f79fc4e3ab53a9369f18acff676948d0d2701f74
Simon <87281874+simonp17@users.noreply.github.com>
2026-01-19 17:17:09 -0800

    Add image width and height to LimelightModel

--- shared/src/main/java/com/team581/config/LimelightModel.java
+++ shared/src/main/java/com/team581/config/LimelightModel.java
@@ -1,7 +1,15 @@
 package com.team581.config;
 
 public enum LimelightModel {
-  THREE,
-  THREEG,
-  FOUR;
+  THREE(640, 480),
+  THREEG(1280, 800),
+  FOUR(1280, 800);
+
+  public final int width;
+  public final int height;
+
+  private LimelightModel(int width, int height) {
+    this.width = width;
+    this.height = height;
+  }
 }
