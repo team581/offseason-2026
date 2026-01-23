9a1ff75a937eed1c541eea6e22b32a703455ba2d
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2026-01-23 12:27:36 -0800

    tune kicker motor for voltage request

--- dumper-bot/src/main/java/frc/robot/shooter/Shooter.java
+++ dumper-bot/src/main/java/frc/robot/shooter/Shooter.java
@@ -117,7 +117,7 @@ public class Shooter extends StateMachineSubsystem<ShooterState> {
                     .withStatorCurrentLimit(100)
                     .withSupplyCurrentLimit(100))
             .withMotorOutput(new MotorOutputConfigs().withNeutralMode(NeutralModeValue.Coast))
-            .withSlot0(new Slot0Configs().withKP(3.0).withKV(0.01).withKS(5.0))
+            .withSlot0(new Slot0Configs().withKP(0.1).withKV(0.128).withKS(0.0))
             .withTorqueCurrent(
                 new TorqueCurrentConfigs()
                     .withPeakForwardTorqueCurrent(200)
