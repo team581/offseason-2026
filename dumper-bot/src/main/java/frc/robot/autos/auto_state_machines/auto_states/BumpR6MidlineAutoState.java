e2a25a9d078a923af7776efd102689a02d625f7e
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2026-01-22 17:46:54 -0800

    add bump r6 midline auto state

---
+++ dumper-bot/src/main/java/frc/robot/autos/auto_state_machines/auto_states/BumpR6MidlineAutoState.java
@@ -0,0 +1,19 @@
+package frc.robot.autos.auto_state_machines.auto_states;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import frc.robot.autos.Points;
+
+public enum BumpR6MidlineAutoState {
+    INTAKING(Points.INTAKE_MIDLINE_RIGHT),
+    SCORE(Points.HUB_CENTER);
+
+public final Points point;
+
+  public Pose2d getPose() {
+    return this.point.getPose();
+  }
+
+  private BumpR6MidlineAutoState(Points point) {
+    this.point = point;
+  }
+}
