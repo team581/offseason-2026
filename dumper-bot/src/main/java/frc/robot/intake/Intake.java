c7e9b46d00732744e84d049bb46f2e1188f263de
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2026-01-23 12:26:41 -0800

    log stator &  supply current & vel of intake

--- dumper-bot/src/main/java/frc/robot/intake/Intake.java
+++ dumper-bot/src/main/java/frc/robot/intake/Intake.java
@@ -5,8 +5,11 @@ import com.ctre.phoenix6.configs.FeedbackConfigs;
 import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import com.team581.util.state_machines.StateMachineSubsystem;
+
+import dev.doglog.DogLog;
 import frc.robot.util.scheduling.SubsystemPriority;
 
 public class Intake extends StateMachineSubsystem<IntakeState> {
@@ -23,7 +26,7 @@ public class Intake extends StateMachineSubsystem<IntakeState> {
                     new CurrentLimitsConfigs()
                         .withStatorCurrentLimit(100)
                         .withSupplyCurrentLimit(100))
-                .withMotorOutput(new MotorOutputConfigs().withNeutralMode(NeutralModeValue.Coast)));
+                .withMotorOutput(new MotorOutputConfigs().withNeutralMode(NeutralModeValue.Coast).withInverted(InvertedValue.Clockwise_Positive)));
     this.motor = motor;
   }
 
@@ -34,6 +37,14 @@ public class Intake extends StateMachineSubsystem<IntakeState> {
       default -> motor.setVoltage(getState().volts);
     }
   }
+
+  @Override
+  protected void whileInState(IntakeState state) {
+    DogLog.log("Intake/StatorCurrent", motor.getStatorCurrent().getValueAsDouble());
+    DogLog.log("Intake/SupplyCurrent", motor.getSupplyCurrent().getValueAsDouble());
+    DogLog.log("Intake/Velocity", motor.getVelocity().getValueAsDouble());
+  }
+
 
   public void intakeRequest() {
     setStateFromRequest(IntakeState.INTAKING);
