2e3eafadf97c8c48f1c935e3050f309bc30bfa38
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2026-01-22 17:46:42 -0800

    update auto points for 2026

--- dumper-bot/src/main/java/frc/robot/autos/Points.java
+++ dumper-bot/src/main/java/frc/robot/autos/Points.java
@@ -5,37 +5,25 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 
 public enum Points {
-  NON_PROCESSOR_SIDE_START_ANGLED(
-      new Point(
-          new Pose2d(0, 0, Rotation2d.fromDegrees(0.0)),
-          new Pose2d(0, 0, Rotation2d.fromDegrees(0.0)))),
 
-  PROCESSOR_SIDE_START_ANGLED(
-      new Point(
-          new Pose2d(0, 0, Rotation2d.fromDegrees(0.0)),
-          new Pose2d(0, 0, Rotation2d.fromDegrees(0.0)))),
+  START_R1_AND_B1(Point.ofRed(new Pose2d(12.925, 2.023, Rotation2d.fromDegrees(180)))),
+  START_R2_AND_B2(Point.ofRed(new Pose2d(12.954, 2.485, Rotation2d.fromDegrees(180)))),
+  START_R3_AND_B3(Point.ofRed(new Pose2d(12.939, 2.97, Rotation2d.fromDegrees(180)))),
+  START_R4_AND_B4(Point.ofRed(new Pose2d(12.939, 5.074, Rotation2d.fromDegrees(180)))),
+  START_R5_AND_B5(Point.ofRed(new Pose2d(12.928, 5.557, Rotation2d.fromDegrees(180)))),
+  START_R6_AND_B6(Point.ofRed(new Pose2d(12.959, 6.02, Rotation2d.fromDegrees(180)))),
 
-  START_R1_AND_B1(Point.ofRed(new Pose2d(0, 0, Rotation2d.fromDegrees(0.0)))),
-  START_R1_AND_B1_FORWARD(Point.ofRed(new Pose2d(0, 0, Rotation2d.fromDegrees(0)))),
 
-  START_R2_AND_B2(Point.ofRed(new Pose2d(0, 0, Rotation2d.fromDegrees(0)))),
-  START_R3_AND_B3(Point.ofRed(new Pose2d(0, 0, Rotation2d.fromDegrees(0)))),
-  START_R3_AND_B3_LEFT_FORWARD(Point.ofRed(new Pose2d(0, 0, Rotation2d.fromDegrees(0)))),
+  INTAKE_MIDLINE_RIGHT(
+     Point.ofRed(new Pose2d(9.64, 5.938, Rotation2d.fromDegrees(180)))),
+  INTAKE_MIDLINE_LEFT(
+     Point.ofRed(new Pose2d(9.64, 2.154, Rotation2d.fromDegrees(180)))),
+  INTAKE_DEPOT(
+     Point.ofRed(new Pose2d(15.469, 2.118, Rotation2d.fromDegrees(0.0)))),
 
-  START_R4_AND_B4(Point.ofRed(new Pose2d(0, 0, Rotation2d.fromDegrees(0)))),
-  START_R5_AND_B5(Point.ofRed(new Pose2d(0, 0, Rotation2d.fromDegrees(0)))),
-  START_R6_AND_B6_FORWARD(Point.ofRed(new Pose2d(0, 0, Rotation2d.fromDegrees(0)))),
-  START_R6_AND_B6(Point.ofRed(new Pose2d(0, 0, Rotation2d.fromDegrees(0.0)))),
-
-  GROUND_INTAKE_NON_PROCESSOR_SIDE_STATION(
-      Point.ofRed(new Pose2d(0, 0, Rotation2d.fromDegrees(0)))),
-  PRE_GROUND_INTAKE_NON_PROCESSOR_SIDE_STATION(
-      Point.ofRed(new Pose2d(0, 0, Rotation2d.fromDegrees(0)))),
-  GROUND_INTAKE_PROCESSOR_SIDE_STATION(Point.ofRed(new Pose2d(0, 0, Rotation2d.fromDegrees(0)))),
-  PRE_GROUND_INTAKE_PROCESSOR_SIDE_STATION(
-      Point.ofRed(new Pose2d(0, 0, Rotation2d.fromDegrees(0)))),
-
-  LOLLIPOP_2(Point.ofRed(new Pose2d(0, 0, Rotation2d.fromDegrees(180.0))));
+  HUB_CENTER(Point.ofRed(new Pose2d(14.505, 4.0, Rotation2d.fromDegrees(180.0)))),
+  HUB_RIGHT(Point.ofRed(new Pose2d(14.303,4.602, Rotation2d.fromDegrees(-130.0)))),
+  HUB_LEFT(Point.ofRed(new Pose2d(14.56,3.034, Rotation2d.fromDegrees(145.1))));
 
   public final Point point;
 
