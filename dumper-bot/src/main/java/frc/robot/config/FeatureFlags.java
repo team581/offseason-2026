02bf14974619947a409c3d6abea35ccae1a45959
Simon <87281874+simonp17@users.noreply.github.com>
2026-01-24 14:47:51 -0800

    Fix import in feature flags

--- dumper-bot/src/main/java/frc/robot/config/FeatureFlags.java
+++ dumper-bot/src/main/java/frc/robot/config/FeatureFlags.java
@@ -1,7 +1,8 @@
 package frc.robot.config;
 
-import com.team581.config.FeatureFlag;
 import java.util.function.BooleanSupplier;
+
+import com.team581.config.FeatureFlag;
 
 public class FeatureFlags {
   public static final BooleanSupplier SHOOT_ON_THE_MOVE = FeatureFlag.of("ShootOnTheMove", true);
