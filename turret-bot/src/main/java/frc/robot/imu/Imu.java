dbe9c787db1309b877170ade27c100b39a0e4f17
Simon <87281874+simonp17@users.noreply.github.com>
2026-01-25 13:22:25 -0800

    Refactor IMU class to remove built-in accelerometer

--- turret-bot/src/main/java/frc/robot/imu/Imu.java
+++ turret-bot/src/main/java/frc/robot/imu/Imu.java
@@ -4,20 +4,14 @@ import com.ctre.phoenix6.swerve.SwerveDrivetrain;
 import com.team581.mechanisms.imu.BaseImuSubsystem;
 import dev.doglog.DogLog;
 import edu.wpi.first.math.filter.LinearFilter;
-import edu.wpi.first.wpilibj.BuiltInAccelerometer;
+import edu.wpi.first.networktables.DoubleSubscriber;
 import frc.robot.util.scheduling.SubsystemPriority;
 
 public class Imu extends BaseImuSubsystem {
-  private final BuiltInAccelerometer builtInAccelerometer = new BuiltInAccelerometer();
-
-  private final LinearFilter builtInXAccelFilter = LinearFilter.movingAverage(10);
-  private final LinearFilter builtInYAccelFilter = LinearFilter.movingAverage(10);
-
   private final LinearFilter pigeonXAccelFilter = LinearFilter.movingAverage(10);
   private final LinearFilter pigeonYAccelFilter = LinearFilter.movingAverage(10);
 
-  private double builtInPrevXAccel = 0.0;
-  private double builtInPrevYAccel = 0.0;
+  private static final DoubleSubscriber COLLISION_G_FORCE_THRESHOLD = DogLog.tunable("Imu/CollisionGForceThreshold", 2.0);
 
   private double pigeonXPrevAccel = 0.0;
   private double pigeonYPrevAccel = 0.0;
@@ -28,9 +22,6 @@ public class Imu extends BaseImuSubsystem {
 
   @Override
   public void collectInputs() {
-    double builtInFilteredXAccel = builtInXAccelFilter.calculate(builtInAccelerometer.getX());
-    double builtInFilteredYAccel = builtInYAccelFilter.calculate(builtInAccelerometer.getY());
-
     double pigeonFilteredXAccel =
         pigeonXAccelFilter.calculate(drivetrain.getPigeon2().getAccelerationX().getValueAsDouble());
     double pigeonFilteredYAccel =
@@ -38,21 +29,18 @@ public class Imu extends BaseImuSubsystem {
 
     // Calculates the jerk in the X and Y directions
     // Divides by .02 because default loop timing is 20ms
-    double builtInXJerk = (builtInFilteredXAccel - builtInPrevXAccel) / 0.02;
-    double builtInYJerk = (builtInFilteredYAccel - builtInPrevYAccel) / 0.02;
-
     double pigeonXJerk = (pigeonFilteredXAccel - pigeonXPrevAccel) / 0.02;
     double pigeonYJerk = (pigeonFilteredYAccel - pigeonYPrevAccel) / 0.02;
 
-    builtInPrevXAccel = builtInFilteredXAccel;
-    builtInPrevYAccel = builtInFilteredYAccel;
 
     pigeonXPrevAccel = pigeonFilteredXAccel;
     pigeonYPrevAccel = pigeonFilteredYAccel;
 
-    DogLog.log("Imu/BuildInAccelerometer/XJerk", builtInXJerk);
-    DogLog.log("Imu/BuildInAccelerometer/YJerk", builtInYJerk);
-    DogLog.log("Imu/BuildInAccelerometer/HypotJerk", Math.hypot(builtInXJerk, builtInYJerk));
+    double pigeonJerk = Math.hypot(pigeonXJerk, pigeonYJerk);
+
+    pigeonGForce = pigeonJerk / 9.81;
+    maxGForceDetected = Math.max(maxGForceDetected, pigeonGForce);
+
 
     DogLog.log("Imu/Pigeon/XJerk", pigeonXJerk);
     DogLog.log("Imu/Pigeon/YJerk", pigeonYJerk);
