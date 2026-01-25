e6078dc7ce0ceceeaab764c87521f518bc653632
Simon <87281874+simonp17@users.noreply.github.com>
2026-01-25 14:20:40 -0800

    Make all constants in TrustFactor tunables

--- shared/src/main/java/com/team581/localization/TrustFactor.java
+++ shared/src/main/java/com/team581/localization/TrustFactor.java
@@ -1,16 +1,15 @@
 package com.team581.localization;
 
+import dev.doglog.DogLog;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.networktables.DoubleSubscriber;
 
 public class TrustFactor {
-  private static final double DISTANCE_TRAVELLED_SCALAR = 1.0;
-  private static final double POST_COLLISION_ADDITION = 5.0;
-
-  private static final double TRUSTWORTHY_THRESHOLD = 1.0;
-
-  private static final double TRUST_FACTOR_TAG_SEEN_DENOMINATOR = 3.0;
-  private static final double TRUST_FACTOR_TAG_SEEN_MAX = 5.0;
-
+  private static final DoubleSubscriber DISTANCE_TRAVELLED_SCALAR = DogLog.tunable("TrustFactor/DistanceTravelledScalar", 1.0);
+  private static final DoubleSubscriber POST_COLLISION_ADDITION = DogLog.tunable("TrustFactor/PostCollisionAddition", 5.0);
+  private static final DoubleSubscriber TRUSTWORTHY_THRESHOLD = DogLog.tunable("TrustFactor/TrustworthyThreshold", 1.0);
+  private static final DoubleSubscriber TRUST_FACTOR_TAG_SEEN_DENOMINATOR = DogLog.tunable("TrustFactor/TrustFactorTagSeenDenominator", 3.0);
+  private static final DoubleSubscriber TRUST_FACTOR_TAG_SEEN_MAX = DogLog.tunable("TrustFactor/TrustFactorTagSeenMax", 5.0);
   private double trustFactor = 0.0;
   private double metersTravelledSinceLastCheck = 0.0;
   private Pose2d lastCheckedPose = Pose2d.kZero;
@@ -20,23 +19,23 @@ public class TrustFactor {
   }
 
   public boolean isTrustworthy() {
-    return trustFactor <= TRUSTWORTHY_THRESHOLD;
+    return trustFactor <= TRUSTWORTHY_THRESHOLD.get();
   }
 
   public void tagSeen() {
     trustFactor =
-        Math.min(trustFactor / TRUST_FACTOR_TAG_SEEN_DENOMINATOR, TRUST_FACTOR_TAG_SEEN_MAX);
+        Math.min(trustFactor / TRUST_FACTOR_TAG_SEEN_DENOMINATOR.get(), TRUST_FACTOR_TAG_SEEN_MAX.get());
   }
 
   public void update(Pose2d robotPose, boolean collisionDetected) {
     metersTravelledSinceLastCheck =
         lastCheckedPose.getTranslation().getDistance(robotPose.getTranslation());
 
-    trustFactor += metersTravelledSinceLastCheck * DISTANCE_TRAVELLED_SCALAR;
+    trustFactor += metersTravelledSinceLastCheck * DISTANCE_TRAVELLED_SCALAR.get();
     lastCheckedPose = robotPose;
 
     if (collisionDetected) {
-      trustFactor += POST_COLLISION_ADDITION;
+      trustFactor += POST_COLLISION_ADDITION.get();
     }
   }
 }
