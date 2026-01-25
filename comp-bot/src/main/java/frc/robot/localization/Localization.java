64f1bb3c857784acbf01f7f4766f2f2cfd10e513
Simon <87281874+simonp17@users.noreply.github.com>
2026-01-25 14:08:50 -0800

    Improve comp bot Localization with Imu and TrustFactor

--- comp-bot/src/main/java/frc/robot/localization/Localization.java
+++ comp-bot/src/main/java/frc/robot/localization/Localization.java
@@ -1,23 +1,32 @@
 package frc.robot.localization;
 
 import com.ctre.phoenix6.Utils;
+import com.team581.localization.TrustFactor;
 import com.team581.util.state_machines.StateMachineSubsystem;
+import com.team581.vision.results.TagResult;
 import dev.doglog.DogLog;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import frc.robot.generated.RobotTunerConstants.TunerSwerveDrivetrain;
+import frc.robot.imu.Imu;
 import frc.robot.swerve.Swerve;
 import frc.robot.util.scheduling.SubsystemPriority;
 
 public class Localization extends StateMachineSubsystem<LocalizationState> {
+
+  private static final double LATENCY_CONSTANT = 0.0;
+
   private final Swerve swerve;
   private final TunerSwerveDrivetrain drivetrain;
+  private final Imu imu;
   private Pose2d robotPose = Pose2d.kZero;
+  private final TrustFactor trustFactor = new TrustFactor();
 
-  public Localization(Swerve swerve, TunerSwerveDrivetrain drivetrain) {
+  public Localization(Swerve swerve, TunerSwerveDrivetrain drivetrain, Imu imu) {
     super(SubsystemPriority.LOCALIZATION, LocalizationState.DEFAULT_STATE);
     this.swerve = swerve;
     this.drivetrain = drivetrain;
+    this.imu = imu;
   }
 
   public Pose2d getLookaheadPose(double lookahead) {
@@ -42,6 +51,10 @@ public class Localization extends StateMachineSubsystem<LocalizationState> {
     return drivetrain.samplePoseAt(newTimestamp).orElseGet(this::getPose);
   }
 
+  public double getTrustFactor() {
+    return trustFactor.get();
+  }
+
   public void resetPose(Pose2d estimatedPose) {
     drivetrain.resetPose(estimatedPose);
   }
@@ -55,8 +68,22 @@ public class Localization extends StateMachineSubsystem<LocalizationState> {
     drivetrain.seedFieldCentric();
   }
 
+  private void ingestTagResult(TagResult result) {
+    var visionPose = result.pose();
+    trustFactor.tagSeen();
+    // if (!vision.seenTagRecentlyForReset()) {
+    //   resetPose(visionPose);
+    // }
+    swerve.drivetrain.addVisionMeasurement(
+        visionPose,
+        Utils.fpgaToCurrentTime(result.timestamp() - (LATENCY_CONSTANT / 1000)),
+        result.standardDevs());
+  }
+
   @Override
   protected void collectInputs() {
     robotPose = drivetrain.getState().Pose;
+
+    trustFactor.update(robotPose, imu.collisionDetected());
   }
 }
