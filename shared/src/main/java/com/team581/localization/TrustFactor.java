d810285414b8ce41d0041ab50936a8bf9f5f7f8e
Simon <87281874+simonp17@users.noreply.github.com>
2026-01-25 14:09:49 -0800

    Improve trust factor calculation

--- shared/src/main/java/com/team581/localization/TrustFactor.java
+++ shared/src/main/java/com/team581/localization/TrustFactor.java
@@ -3,9 +3,15 @@ package com.team581.localization;
 import edu.wpi.first.math.geometry.Pose2d;
 
 public class TrustFactor {
-  private static final double MIN_TRUST_FACTOR = 0.01;
+  private static final double DISTANCE_TRAVELLED_SCALAR = 1.0;
+  private static final double POST_COLLISION_ADDITION = 5.0;
 
-  private double trustFactor = 0.1;
+  private static final double TRUSTWORTHY_THRESHOLD = 1.0;
+
+  private static final double TRUST_FACTOR_TAG_SEEN_DENOMINATOR = 3.0;
+  private static final double TRUST_FACTOR_TAG_SEEN_MAX = 5.0;
+
+  private double trustFactor = 0.0;
   private double metersTravelledSinceLastCheck = 0.0;
   private Pose2d lastCheckedPose = Pose2d.kZero;
 
@@ -13,14 +19,24 @@ public class TrustFactor {
     return trustFactor;
   }
 
-  public void tagSeen(Pose2d robotPose) {
-    trustFactor = 0.1;
-    lastCheckedPose = robotPose;
+  public boolean isTrustworthy() {
+    return trustFactor <= TRUSTWORTHY_THRESHOLD;
   }
 
-  public void update(Pose2d robotPose) {
+  public void tagSeen() {
+    trustFactor =
+        Math.min(trustFactor / TRUST_FACTOR_TAG_SEEN_DENOMINATOR, TRUST_FACTOR_TAG_SEEN_MAX);
+  }
+
+  public void update(Pose2d robotPose, boolean collisionDetected) {
     metersTravelledSinceLastCheck =
         lastCheckedPose.getTranslation().getDistance(robotPose.getTranslation());
-    trustFactor += Math.max(MIN_TRUST_FACTOR, trustFactor * metersTravelledSinceLastCheck * 0.001);
+
+    trustFactor += metersTravelledSinceLastCheck * DISTANCE_TRAVELLED_SCALAR;
+    lastCheckedPose = robotPose;
+
+    if (collisionDetected) {
+      trustFactor += POST_COLLISION_ADDITION;
+    }
   }
 }
