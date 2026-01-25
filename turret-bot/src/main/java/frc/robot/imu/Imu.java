0bcac3a12c79aa139754c1f73d45bc4537f4b878
Simon <87281874+simonp17@users.noreply.github.com>
2026-01-25 12:42:00 -0800

    Copy over IMU acceleration logs to turret bot

--- turret-bot/src/main/java/frc/robot/imu/Imu.java
+++ turret-bot/src/main/java/frc/robot/imu/Imu.java
@@ -2,10 +2,61 @@ package frc.robot.imu;
 
 import com.ctre.phoenix6.swerve.SwerveDrivetrain;
 import com.team581.mechanisms.imu.BaseImuSubsystem;
+
+import dev.doglog.DogLog;
+import edu.wpi.first.math.filter.LinearFilter;
+import edu.wpi.first.wpilibj.BuiltInAccelerometer;
 import frc.robot.util.scheduling.SubsystemPriority;
 
 public class Imu extends BaseImuSubsystem {
+  private final BuiltInAccelerometer builtInAccelerometer = new BuiltInAccelerometer();
+
+  private final LinearFilter builtInXAccelFilter = LinearFilter.movingAverage(10);
+  private final LinearFilter builtInYAccelFilter = LinearFilter.movingAverage(10);
+
+  private final LinearFilter pigeonXAccelFilter = LinearFilter.movingAverage(10);
+  private final LinearFilter pigeonYAccelFilter = LinearFilter.movingAverage(10);
+
+  private double builtInPrevXAccel = 0.0;
+  private double builtInPrevYAccel = 0.0;
+
+  private double pigeonXPrevAccel = 0.0;
+  private double pigeonYPrevAccel = 0.0;
+
   public Imu(SwerveDrivetrain<?, ?, ?> drivetrain) {
     super(SubsystemPriority.IMU, drivetrain);
+  }
+
+  @Override
+  public void collectInputs() {
+    double builtInFilteredXAccel = builtInXAccelFilter.calculate(builtInAccelerometer.getX());
+    double builtInFilteredYAccel = builtInYAccelFilter.calculate(builtInAccelerometer.getY());
+
+    double pigeonFilteredXAccel =
+        pigeonXAccelFilter.calculate(drivetrain.getPigeon2().getAccelerationX().getValueAsDouble());
+    double pigeonFilteredYAccel =
+        pigeonYAccelFilter.calculate(drivetrain.getPigeon2().getAccelerationY().getValueAsDouble());
+
+    // Calculates the jerk in the X and Y directions
+    // Divides by .02 because default loop timing is 20ms
+    double builtInXJerk = (builtInFilteredXAccel - builtInPrevXAccel) / 0.02;
+    double builtInYJerk = (builtInFilteredYAccel - builtInPrevYAccel) / 0.02;
+
+    double pigeonXJerk = (pigeonFilteredXAccel - pigeonXPrevAccel) / 0.02;
+    double pigeonYJerk = (pigeonFilteredYAccel - pigeonYPrevAccel) / 0.02;
+
+    builtInPrevXAccel = builtInFilteredXAccel;
+    builtInPrevYAccel = builtInFilteredYAccel;
+
+    pigeonXPrevAccel = pigeonFilteredXAccel;
+    pigeonYPrevAccel = pigeonFilteredYAccel;
+
+    DogLog.log("Imu/BuildInAccelerometer/XJerk", builtInXJerk);
+    DogLog.log("Imu/BuildInAccelerometer/YJerk", builtInYJerk);
+    DogLog.log("Imu/BuildInAccelerometer/HypotJerk", Math.hypot(builtInXJerk, builtInYJerk));
+
+    DogLog.log("Imu/Pigeon/XJerk", pigeonXJerk);
+    DogLog.log("Imu/Pigeon/YJerk", pigeonYJerk);
+    DogLog.log("Imu/Pigeon/HypotAccel", Math.hypot(pigeonXJerk, pigeonYJerk));
   }
 }
