12198e02106cc7284fa8f90fff926f29feb96da8
Simon <87281874+simonp17@users.noreply.github.com>
2026-01-25 19:06:31 -0800

    Add DSOption for disabling vision on comp

--- comp-bot/src/main/java/frc/robot/config/DSOptions.java
+++ comp-bot/src/main/java/frc/robot/config/DSOptions.java
@@ -1,10 +1,11 @@
 package frc.robot.config;
 
 import com.team581.config.DSOption;
+
 import edu.wpi.first.networktables.BooleanSubscriber;
 
 public final class DSOptions {
-  public static final BooleanSubscriber SENSOR_BROKEN = DSOption.of("SensorBroken", false);
-
+  public static final BooleanSubscriber SENSOR_OVERRIDE = DSOption.of("SensorOverride", false);
+  public static final BooleanSubscriber VISION_DISABLE = DSOption.of("VisionDisable", false);
   private DSOptions() {}
 }
