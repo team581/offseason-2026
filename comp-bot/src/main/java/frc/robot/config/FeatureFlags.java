cd12a942d63416d46737ad67af8122520ac4ed87
Simon <87281874+simonp17@users.noreply.github.com>
2026-01-25 19:16:04 -0800

    Add hub tags only Feature Flag

--- comp-bot/src/main/java/frc/robot/config/FeatureFlags.java
+++ comp-bot/src/main/java/frc/robot/config/FeatureFlags.java
@@ -1,5 +1,11 @@
 package frc.robot.config;
 
+import com.team581.config.FeatureFlag;
+import java.util.function.BooleanSupplier;
+
 public class FeatureFlags {
+  public static final BooleanSupplier VISION_HUB_TAGS_FILTER =
+      FeatureFlag.of("OnlyUseHubTags", true);
+
   private FeatureFlags() {}
 }
