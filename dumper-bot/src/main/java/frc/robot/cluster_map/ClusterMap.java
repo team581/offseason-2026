ae9099af372ab2a0d80ccdfcaf1504ba5c14effd
Simon <87281874+simonp17@users.noreply.github.com>
2026-01-26 11:07:39 -0800

    Format

--- dumper-bot/src/main/java/frc/robot/cluster_map/ClusterMap.java
+++ dumper-bot/src/main/java/frc/robot/cluster_map/ClusterMap.java
@@ -1,16 +1,11 @@
 package frc.robot.cluster_map;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
 import static java.util.Comparator.comparingDouble;
-import java.util.Optional;
 
 import com.team581.math.GamePieceDetectionCalculator;
 import com.team581.math.MathHelpers;
 import com.team581.util.state_machines.StateMachineSubsystem;
 import com.team581.vision.results.GamePieceResult;
-
 import dev.doglog.DogLog;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -24,6 +19,10 @@ import frc.robot.util.scheduling.SubsystemPriority;
 import frc.robot.vision.limelight.Limelight;
 import frc.robot.vision.limelight.LimelightHelpers;
 import frc.robot.vision.limelight.LimelightState;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Optional;
 
 public class ClusterMap extends StateMachineSubsystem<ClusterMapState> {
   private static final double SAME_CLUSTER_DETECTION_THRESHOLD_METERS = 1.0;
@@ -174,7 +173,7 @@ public class ClusterMap extends StateMachineSubsystem<ClusterMapState> {
 
     if (match.isPresent()) {
       var existingCluster = match.orElseThrow().clusterTranslation();
-      var health = match.orElseThrow().health()+1;
+      var health = match.orElseThrow().health() + 1;
       var blendedPose = existingCluster.interpolate(visionCluster, 1 / health);
       clusterMap.add(new ClusterMapElement(newClusterExpiry, blendedPose, health));
       clusterMap.remove(match.orElseThrow());
