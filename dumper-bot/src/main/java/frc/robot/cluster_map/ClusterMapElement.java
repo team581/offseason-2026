75f29933ca86a85fa712d7595184b39567d9d82b
Simon <87281874+simonp17@users.noreply.github.com>
2026-01-25 19:57:32 -0800

    Interpolate matching cluster detections when adding them to map

--- dumper-bot/src/main/java/frc/robot/cluster_map/ClusterMapElement.java
+++ dumper-bot/src/main/java/frc/robot/cluster_map/ClusterMapElement.java
@@ -2,4 +2,4 @@ package frc.robot.cluster_map;
 
 import edu.wpi.first.math.geometry.Translation2d;
 
-public record ClusterMapElement(double expiresAt, Translation2d clusterTranslation) {}
+public record ClusterMapElement(double expiresAt, Translation2d clusterTranslation, double health) {}
