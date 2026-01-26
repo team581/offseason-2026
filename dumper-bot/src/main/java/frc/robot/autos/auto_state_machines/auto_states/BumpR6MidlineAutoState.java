d7403954987477d8e20f44db44857bb251571886
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2026-01-25 19:37:53 -0800

    new auto states

--- dumper-bot/src/main/java/frc/robot/autos/auto_state_machines/auto_states/BumpR6MidlineAutoState.java
+++ dumper-bot/src/main/java/frc/robot/autos/auto_state_machines/auto_states/BumpR6MidlineAutoState.java
@@ -1,19 +1,10 @@
 package frc.robot.autos.auto_state_machines.auto_states;
 
-import edu.wpi.first.math.geometry.Pose2d;
-import frc.robot.autos.Points;
-
 public enum BumpR6MidlineAutoState {
-  INTAKING(Points.INTAKE_MIDLINE_RIGHT),
-  SCORE(Points.HUB_CENTER);
-
-  public final Points point;
-
-  private BumpR6MidlineAutoState(Points point) {
-    this.point = point;
-  }
-
-  public Pose2d getPose() {
-    return this.point.getPose();
-  }
+  SEGMENT_1_DRIVE_TO_MIDLINE,
+  INTAKE,
+  SEGMENT_2_DRIVE_ACROSS_MIDLINE,
+  SEGMENT_3_DRIVE_BACK,
+  SEGMENT_4_DRIVE_TO_SHOOT,
+  DONE;
 }
