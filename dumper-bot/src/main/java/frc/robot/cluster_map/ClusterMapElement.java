109e5791c5e0b6907bfa2871230e8dfdfa009295
Simon <87281874+simonp17@users.noreply.github.com>
2026-01-25 19:58:04 -0800

    Format

--- dumper-bot/src/main/java/frc/robot/cluster_map/ClusterMapElement.java
+++ dumper-bot/src/main/java/frc/robot/cluster_map/ClusterMapElement.java
@@ -2,4 +2,5 @@ package frc.robot.cluster_map;
 
 import edu.wpi.first.math.geometry.Translation2d;
 
-public record ClusterMapElement(double expiresAt, Translation2d clusterTranslation, double health) {}
+public record ClusterMapElement(
+    double expiresAt, Translation2d clusterTranslation, double health) {}
