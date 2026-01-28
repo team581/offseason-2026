8ac7ad64a920768f333f33fbb7500a295f2b1681
Simon <87281874+simonp17@users.noreply.github.com>
2026-01-27 17:36:18 -0800

    Add rotational check to cluster map

--- dumper-bot/src/main/java/frc/robot/cluster_map/ClusterMap.java
+++ dumper-bot/src/main/java/frc/robot/cluster_map/ClusterMap.java
@@ -1,12 +1,18 @@
 package frc.robot.cluster_map;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import static java.util.Comparator.comparingDouble;
+import java.util.Optional;
 
 import com.team581.math.GamePieceDetectionCalculator;
 import com.team581.math.MathHelpers;
 import com.team581.util.state_machines.StateMachineSubsystem;
 import com.team581.vision.results.GamePieceResult;
+
 import dev.doglog.DogLog;
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
@@ -19,10 +25,6 @@ import frc.robot.util.scheduling.SubsystemPriority;
 import frc.robot.vision.limelight.Limelight;
 import frc.robot.vision.limelight.LimelightHelpers;
 import frc.robot.vision.limelight.LimelightState;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Optional;
 
 public class ClusterMap extends StateMachineSubsystem<ClusterMapState> {
   private static final double SAME_CLUSTER_DETECTION_THRESHOLD_METERS = 1.0;
@@ -67,7 +69,10 @@ public class ClusterMap extends StateMachineSubsystem<ClusterMapState> {
             .min(bestClusterComparator);
     if (bestCluster.isPresent()) {
       var rotation =
-          MathHelpers.getDriveDirection(bestCluster.orElseThrow(), localization.getPose());
+          MathHelpers.getDriveDirection(localization.getPose(), bestCluster.orElseThrow());
+      if (!MathUtil.isNear(localization.getPose().getRotation().getDegrees(), rotation.getDegrees(),45,-180,180)) {
+        return Optional.empty();
+      }
       var clusterPoseWithIntakeRotation =
           new Pose2d(bestCluster.orElseThrow().getTranslation(), rotation);
       DogLog.log("ClusterMap/BestClusterPose", clusterPoseWithIntakeRotation);
