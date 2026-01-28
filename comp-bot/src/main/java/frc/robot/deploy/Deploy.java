e72adc395ea67cee39f437fe8c627cb6b4688b91
Jordan <117786700+jordanjcoderman@users.noreply.github.com>
2026-01-27 18:59:13 -0800

    Finalized Deploy System Added Angles to States and Created Variables

--- comp-bot/src/main/java/frc/robot/deploy/Deploy.java
+++ comp-bot/src/main/java/frc/robot/deploy/Deploy.java
@@ -17,6 +17,9 @@ public class Deploy extends StateMachineSubsystem<DeployState> {
   private final TalonFX motor;
   private final PositionVoltage positionVoltageRequest =
       new PositionVoltage(0).withEnableFOC(false);
+      private final double homingEndAngle = 0;
+      private final double homingVoltage = 0;
+      //TODO: Find angle eventually
 
   public Deploy(TalonFX motor) {
     super(SubsystemPriority.DEPLOY, DeployState.UNHOMED);
@@ -63,7 +66,7 @@ public class Deploy extends StateMachineSubsystem<DeployState> {
     return switch (currentState) {
       case HOMING -> {
         if (motor.getStatorCurrent().getValueAsDouble() > 20) {
-          // TODO(@Anju501): reset the encoder to a homed position (this is some angle we dont know
+          motor.setPosition(0);// TODO: reset the encoder to a homed position (this is some angle we dont know
           // yet)
           yield DeployState.STOWED;
         } else {
@@ -79,12 +82,9 @@ public class Deploy extends StateMachineSubsystem<DeployState> {
     switch (newState) {
       case UNHOMED -> motor.disable();
       case HOMING -> motor.setVoltage(0);
-      case INTAKE -> {
-        motor.setControl(positionVoltageRequest.withPosition(Units.degreesToRotations(0)));
-      }
-      case STOWED -> {
-        motor.setControl(positionVoltageRequest.withPosition(Units.degreesToRotations(0)));
-      }
+      default -> motor.setControl(positionVoltageRequest.withPosition(Units.degreesToRotations(getState().angle)));
+
     }
   }
 }
+
--- comp-bot/src/main/java/frc/robot/deploy/DeployState.java
+++ comp-bot/src/main/java/frc/robot/deploy/DeployState.java
@@ -1,8 +1,14 @@
 package frc.robot.deploy;
 
 public enum DeployState {
-  INTAKE,
-  STOWED,
-  HOMING,
-  UNHOMED;
+  INTAKE(0),
+  STOWED(0),
+  HOMING(0),
+  UNHOMED(0);
+
+  public final double angle;
+
+  private DeployState(double angle) {
+    this.angle = angle;
+  }
 }
