a84b795c977b88e79e218cb523ef22146c58f6a1
Simon <87281874+simonp17@users.noreply.github.com>
2026-01-27 18:46:00 -0800

    Start integration test

---
+++ dumper-bot/src/main/java/frc/robot/autos/auto_state_machines/auto_states/IntegrationTestState.java
@@ -0,0 +1,10 @@
+package frc.robot.autos.auto_state_machines.auto_states;
+
+public enum IntegrationTestState {
+    DRIVE_TO_START,
+    CLOSE_CENTERED_WITH_HUB,
+    BACK_CENTERED_WITH_HUB,
+    RIGHT_TRENCH,
+    SHOOT_ON_MOVE_RIGHT_TRENCH_TO_MIDDLE_CENTERED_WITH_HUB,
+    PAUSE;
+}
