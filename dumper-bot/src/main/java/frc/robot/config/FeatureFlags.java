f4fd197fca4a1b8f7335b022819c28b82b5c8eed
Simon <87281874+simonp17@users.noreply.github.com>
2026-01-27 17:36:32 -0800

    Change cluster map to default to false

--- dumper-bot/src/main/java/frc/robot/config/FeatureFlags.java
+++ dumper-bot/src/main/java/frc/robot/config/FeatureFlags.java
@@ -1,13 +1,14 @@
 package frc.robot.config;
 
-import com.team581.config.FeatureFlag;
 import java.util.function.BooleanSupplier;
+
+import com.team581.config.FeatureFlag;
 
 public class FeatureFlags {
   public static final BooleanSupplier SHOOT_ON_THE_MOVE = FeatureFlag.of("ShootOnTheMove", true);
   public static final BooleanSupplier VISION_HUB_TAGS_FILTER =
       FeatureFlag.of("OnlyUseHubTags", true);
-  public static final BooleanSupplier CLUSTER_MAP = FeatureFlag.of("ClusterMapEnabled", true);
+  public static final BooleanSupplier CLUSTER_MAP = FeatureFlag.of("ClusterMapEnabled", false);
   public static final BooleanSupplier INTAKE_WALL_SNAPS = FeatureFlag.of("IntakeWallSnaps", false);
   public static final BooleanSupplier INTAKE_DIRECTIONAL_SNAPS =
       FeatureFlag.of("IntakeDirectionalSnaps", false);
