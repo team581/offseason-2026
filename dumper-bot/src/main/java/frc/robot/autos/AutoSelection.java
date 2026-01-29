a7d6224da4804a60102ae75c4cbbe0fa7a492bbf
Simon <87281874+simonp17@users.noreply.github.com>
2026-01-28 20:35:34 -0800

    Add integration test to auto selector

--- dumper-bot/src/main/java/frc/robot/autos/AutoSelection.java
+++ dumper-bot/src/main/java/frc/robot/autos/AutoSelection.java
@@ -4,12 +4,14 @@ import com.team581.autos.AutoSelectionBase;
 import com.team581.trailblazer.Trailblazer;
 import frc.robot.autos.auto_state_machines.BumpR6MidlineAuto;
 import frc.robot.autos.auto_state_machines.DoNothingAuto;
+import frc.robot.autos.auto_state_machines.IntegrationTest;
 import frc.robot.robot_manager.RobotManager;
 import java.util.function.BiFunction;
 
 public enum AutoSelection implements AutoSelectionBase {
   DO_NOTHING(DoNothingAuto::new),
-  BUMP_MIDLINE_AUTO(BumpR6MidlineAuto::new);
+  BUMP_MIDLINE_AUTO(BumpR6MidlineAuto::new),
+  INTEGRATION_TEST(IntegrationTest::new);
 
   public final BiFunction<RobotManager, Trailblazer, BaseImperativeAuto<?>> auto;
 
