1e72e4246e9fd971687e2eda910ad6dbfdaf240e
Jonah Snider <jonah@jonahsnider.com>
2026-01-28 21:13:10 -0800

    Update PoseErrorTolerance to have #atTranslation() and #atRotation()
    Signed-off-by: Jonah Snider <jonah@jonahsnider.com>

--- shared/src/main/java/com/team581/math/PoseErrorTolerance.java
+++ shared/src/main/java/com/team581/math/PoseErrorTolerance.java
@@ -3,6 +3,7 @@ package com.team581.math;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 
 public record PoseErrorTolerance(double linearErrorTolerance, double angularErrorTolerance) {
   public PoseErrorTolerance(double linearErrorTolerance, Rotation2d angularErrorTolerance) {
@@ -10,16 +11,18 @@ public record PoseErrorTolerance(double linearErrorTolerance, double angularErro
   }
 
   public boolean atPose(Pose2d expected, Pose2d actual) {
-    var linearError = expected.getTranslation().getDistance(actual.getTranslation());
+    return atTranslation(expected.getTranslation(), actual.getTranslation())
+        && atRotation(expected.getRotation(), actual.getRotation());
+  }
 
-    // Linear error within tolerance
-    return MathUtil.isNear(0, linearError, linearErrorTolerance)
-        // Rotation error within tolerance
-        && MathUtil.isNear(
-            expected.getRotation().getDegrees(),
-            actual.getRotation().getDegrees(),
-            angularErrorTolerance,
-            -180,
-            180);
+  public boolean atTranslation(Translation2d expected, Translation2d actual) {
+    var linearError = expected.getDistance(actual);
+
+    return MathUtil.isNear(0, linearError, linearErrorTolerance);
+  }
+
+  public boolean atRotation(Rotation2d expected, Rotation2d actual) {
+    return MathUtil.isNear(
+        expected.getDegrees(), actual.getDegrees(), angularErrorTolerance, -180, 180);
   }
 }
