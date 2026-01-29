9b1aafbe162172c865c977aa5e7c2f5e566912f3
Jonah Snider <jonah@jonahsnider.com>
2026-01-28 21:02:41 -0800

    Add test auto to comp-bot
    Signed-off-by: Jonah Snider <jonah@jonahsnider.com>

--- comp-bot/src/main/java/frc/robot/autos/AutoSelection.java
+++ comp-bot/src/main/java/frc/robot/autos/AutoSelection.java
@@ -3,11 +3,13 @@ package frc.robot.autos;
 import com.team581.autos.AutoSelectionBase;
 import com.team581.trailblazer.Trailblazer;
 import frc.robot.autos.auto_state_machines.DoNothingAuto;
+import frc.robot.autos.auto_state_machines.TestAuto;
 import frc.robot.robot_manager.RobotManager;
 import java.util.function.BiFunction;
 
 public enum AutoSelection implements AutoSelectionBase {
-  DO_NOTHING(DoNothingAuto::new);
+  DO_NOTHING(DoNothingAuto::new),
+  TEST(TestAuto::new);
 
   public final BiFunction<RobotManager, Trailblazer, BaseImperativeAuto<?>> auto;
 
---
+++ comp-bot/src/main/java/frc/robot/autos/auto_state_machines/TestAutoState.java
@@ -0,0 +1,10 @@
+package frc.robot.autos.auto_state_machines;
+
+public enum TestAutoState {
+  SEGMENT_1_STRAIGHT,
+  SEGMENT_2_TURN,
+  SEGMENT_3_CONSTRAINED,
+  SEGMENT_4_MULTI_WAYPOINT,
+  SEGMENT_5_CIRCLE,
+  DONE;
+}
