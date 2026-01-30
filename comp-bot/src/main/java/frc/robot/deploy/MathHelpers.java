50372523fc13a1338a4a1d722fd0569096969d64
Jordan <117786700+jordanjcoderman@users.noreply.github.com>
2026-01-29 17:33:31 -0800

    Deploy System Fixed Homing Routine & Added Clamp Angle

---
+++ comp-bot/src/main/java/frc/robot/deploy/MathHelpers.java
@@ -0,0 +1,10 @@
+package frc.robot.deploy;
+
+public class MathHelpers {
+
+    public static Object angleModulus(double deployAngle) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'angleModulus'");
+    }
+
+}
