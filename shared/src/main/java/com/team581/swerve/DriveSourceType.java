95c8b7321fa62cc43311c0d4b366cd5a3eda5031
Jonah Snider <jonah@jonahsnider.com>
2026-01-30 16:08:52 -0800

    Add comments for DriveSourceType
    Signed-off-by: Jonah Snider <jonah@jonahsnider.com>

--- shared/src/main/java/com/team581/swerve/DriveSourceType.java
+++ shared/src/main/java/com/team581/swerve/DriveSourceType.java
@@ -1,6 +1,15 @@
 package com.team581.swerve;
 
+/** Describes how the drive source should be interpreted. */
 public enum DriveSourceType {
+  /**
+   * This drive source outputs velocities that should be followed open loop. They are field-relative
+   * from the perspective of the driver.
+   */
   DRIVER_PERSPECTIVE_OPEN_LOOP,
+  /**
+   * This drive source outputs velocities that should be followed closed loop. They are totally
+   * field-relative, regardless of alliance.
+   */
   FIELD_CENTRIC_CLOSED_LOOP;
 }
