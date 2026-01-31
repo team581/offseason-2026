03f9e8f6cc8cd711262c98b454e85ad4288d80df
rhetorr <140564619+rhetorr@users.noreply.github.com>
2026-01-31 12:21:22 -0800

    Move deploy configs to separate file

--- comp-bot/src/main/java/frc/robot/deploy/Deploy.java
+++ comp-bot/src/main/java/frc/robot/deploy/Deploy.java
@@ -1,13 +1,7 @@
 package frc.robot.deploy;
 
-import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
-import com.ctre.phoenix6.configs.FeedbackConfigs;
-import com.ctre.phoenix6.configs.MotorOutputConfigs;
-import com.ctre.phoenix6.configs.Slot0Configs;
-import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.NeutralModeValue;
 import com.team581.util.state_machines.StateMachineSubsystem;
 import com.team581.util.tuning.TunablePid;
 import edu.wpi.first.math.MathUtil;
@@ -15,14 +9,10 @@ import edu.wpi.first.math.util.Units;
 import frc.robot.util.scheduling.SubsystemPriority;
 
 public class Deploy extends StateMachineSubsystem<DeployState> {
-  private static final double MAX_ANGLE = 90;
-  private static final double MIN_ANGLE = 0;
   // TODO: These are just place holders we should update these when we do find out these angle
   private final TalonFX motor;
   private final PositionVoltage positionVoltageRequest =
       new PositionVoltage(0).withEnableFOC(false);
-  private final double homingEndAngle = 0;
-  private final double homingVoltage = 0;
 
   // TODO: Find angle eventually
 
@@ -30,16 +20,9 @@ public class Deploy extends StateMachineSubsystem<DeployState> {
     super(SubsystemPriority.DEPLOY, DeployState.UNHOMED);
     this.motor = motor;
 
-    var configs =
-        new TalonFXConfiguration()
-            .withFeedback(new FeedbackConfigs().withSensorToMechanismRatio(1))
-            .withMotorOutput(new MotorOutputConfigs().withNeutralMode(NeutralModeValue.Brake))
-            .withCurrentLimits(
-                new CurrentLimitsConfigs().withStatorCurrentLimit(1).withStatorCurrentLimit(1))
-            .withSlot0(new Slot0Configs().withKP(0).withKV(0).withKG(0));
-    motor.getConfigurator().apply(configs);
+    motor.getConfigurator().apply(DeployConfig.MOTOR_CONFIG);
 
-    TunablePid.register("Deploy", motor, configs);
+    TunablePid.register("Deploy", motor, DeployConfig.MOTOR_CONFIG);
   }
 
   public void intakeRequest() {
@@ -84,17 +67,18 @@ public class Deploy extends StateMachineSubsystem<DeployState> {
   }
 
   private static double clamp(double deployAngle) {
-    return MathUtil.clamp(deployAngle, MIN_ANGLE, MAX_ANGLE);
+    return MathUtil.clamp(deployAngle, DeployConfig.MIN_ANGLE, DeployConfig.MAX_ANGLE);
   }
 
   @Override
   protected void afterTransition(DeployState newState) {
     switch (newState) {
       case UNHOMED -> motor.disable();
-      case HOMING -> motor.setVoltage(homingVoltage);
+      case HOMING -> motor.setVoltage(DeployConfig.HOMING_VOLTAGE);
       default ->
           motor.setControl(
-              positionVoltageRequest.withPosition(Units.degreesToRotations(clamp(homingEndAngle))));
+              positionVoltageRequest.withPosition(
+                  Units.degreesToRotations(clamp(DeployConfig.HOMING_END_ANGLE))));
     }
   }
 }
