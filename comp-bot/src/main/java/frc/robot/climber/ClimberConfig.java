af3e48695677648aaf3d4fe91cf0c9fe41affa1c
Jordan <117786700+jordanjcoderman@users.noreply.github.com>
2026-02-05 18:57:59 -0800

    Climber Config added and subsytem

--- comp-bot/src/main/java/frc/robot/climber/ClimberConfig.java
+++ comp-bot/src/main/java/frc/robot/climber/ClimberConfig.java
@@ -1,13 +1,29 @@
 package frc.robot.climber;
 
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.FeedbackConfigs;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 import com.team581.autos.Point;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 
 public class ClimberConfig {
+  public static final TalonFXConfiguration MOTOR_CONFIG =
+      new TalonFXConfiguration()
+          .withFeedback(new FeedbackConfigs().withSensorToMechanismRatio(1))
+          .withMotorOutput(new MotorOutputConfigs().withNeutralMode(NeutralModeValue.Brake))
+          .withCurrentLimits(
+              new CurrentLimitsConfigs().withStatorCurrentLimit(30).withStatorCurrentLimit(30))
+          .withSlot0(new Slot0Configs().withKP(150.0).withKV(0.0).withKG(0.0));
+
   // TODO: UPDATE WITH REAL GOAL LOCATIONS FROM CAD
   public static final Point CLIMB_LEFT_LOCATION =
       Point.ofRed(new Pose2d(15.0, 1.0, Rotation2d.kZero));
   public static final Point CLIMB_RIGHT_LOCATION =
       Point.ofRed(new Pose2d(15.0, 7.0, Rotation2d.kZero));
+
+      public static final double TOLERANCE = 0.1;
 }
