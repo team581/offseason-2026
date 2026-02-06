7df146ec83e4f42405a942d136fecc6e0eed1c99
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2026-02-05 20:04:34 -0800

    TrenchRShootClimb2Auto path tested in sim

--- comp-bot/src/main/java/frc/robot/autos/auto_state_machines/TrenchRShootClimb2Auto.java
+++ comp-bot/src/main/java/frc/robot/autos/auto_state_machines/TrenchRShootClimb2Auto.java
@@ -11,48 +11,53 @@ import frc.robot.autos.BaseImperativeAuto;
 import frc.robot.autos.auto_state_machines.auto_state.TrenchRShootClimb2AutoState;
 import frc.robot.robot_manager.RobotManager;
 
-@SuppressWarnings("unused")
 public class TrenchRShootClimb2Auto extends BaseImperativeAuto<TrenchRShootClimb2AutoState> {
 
   private final AutoSegment driveToMidline =
       Trailblazer.segment(
-              AutoPoint.ofRed(new Pose2d(12.2, 7.4, Rotation2d.kZero)),
-              AutoPoint.ofRed(new Pose2d(11.912, 7.4, Rotation2d.kZero)),
-              AutoPoint.ofRed(new Pose2d(10.1, 7.4, Rotation2d.kZero)),
-              AutoPoint.ofRed(new Pose2d(9.32, 7.4, Rotation2d.kZero)))
+              AutoPoint.ofRed(new Pose2d(12.0, 7.4, Rotation2d.kCW_90deg)),
+              AutoPoint.ofRed(new Pose2d(10.682, 7.024, Rotation2d.kCW_90deg)),
+              AutoPoint.ofRed(new Pose2d(9.32, 5.859, Rotation2d.kCW_90deg)))
           .untilFinished(new PoseErrorTolerance(0.05, 3));
 
   private final AutoSegment intakeAcrossMidline =
       Trailblazer.segment(
-              AutoPoint.ofRed(new Pose2d(9.32, 6.0, Rotation2d.kCW_90deg)),
-              AutoPoint.ofRed(new Pose2d(9.32, 5.8, Rotation2d.kCW_90deg)),
               AutoPoint.ofRed(new Pose2d(9.32, 4.341, Rotation2d.kCW_90deg)),
               AutoPoint.ofRed(new Pose2d(9.32, 2.885, Rotation2d.kCW_90deg)))
           .withLinearConstraints(3, 3)
           .untilFinished(new PoseErrorTolerance(0.05, 3));
 
   private final AutoSegment driveBack =
-      // update for trench
       Trailblazer.segment(
               AutoPoint.ofRed(new Pose2d(9.32, 2.885, Rotation2d.kCW_90deg)),
               AutoPoint.ofRed(new Pose2d(9.32, 4.341, Rotation2d.kCW_90deg)),
-              AutoPoint.ofRed(new Pose2d(9.32, 5.6, Rotation2d.k180deg)),
-              AutoPoint.ofRed(new Pose2d(11.912, 5.6, Rotation2d.k180deg)),
-              AutoPoint.ofRed(new Pose2d(12.9, 5.6, Rotation2d.k180deg)))
+              AutoPoint.ofRed(new Pose2d(9.32, 5.859, Rotation2d.kCW_90deg)),
+              AutoPoint.ofRed(new Pose2d(10.682, 7.024, Rotation2d.kCW_90deg)),
+              AutoPoint.ofRed(new Pose2d(12.0, 7.4, Rotation2d.kCW_90deg)))
           .withLinearConstraints(3, 3)
           .untilFinished(new PoseErrorTolerance(0.05, 3));
 
-  private final AutoSegment driveToShoot =
-      // update for this auto
-      Trailblazer.segment(AutoPoint.ofRed(Pose2d.kZero))
-          .withLinearConstraints(0, 0)
-          .untilFinished();
+  private final AutoSegment driveToShootOne =
+      Trailblazer.segment(
+         AutoPoint.ofRed(new Pose2d(12.3, 7.4, Rotation2d.kCW_90deg)),
+         AutoPoint.ofRed(new Pose2d(13.0, 7.4, Rotation2d.kCW_90deg)),
+        AutoPoint.ofRed(new Pose2d(14.0, 7.4, Rotation2d.kCW_90deg))
+          .withLinearConstraints(3, 3))
+          .untilFinished(new PoseErrorTolerance(0.05, 3));
+
+  private final AutoSegment driveToShootTwo =
+      Trailblazer.segment(
+         AutoPoint.ofRed(new Pose2d(12.3, 7.4, Rotation2d.kCW_90deg)),
+         AutoPoint.ofRed(new Pose2d(13.0, 7.4, Rotation2d.kCW_90deg)),
+        AutoPoint.ofRed(new Pose2d(13.24, 7.4, Rotation2d.kCW_90deg)))
+          .withLinearConstraints(3, 3)
+          .untilFinished(new PoseErrorTolerance(0.05, 3));
 
   private final AutoSegment driveToClimb =
-      // update
       Trailblazer.segment(
-              AutoPoint.ofRed(new Pose2d(13.751, 5.6, Rotation2d.fromDegrees(-134.0))),
-              AutoPoint.ofRed(new Pose2d(14.305, 4.797, Rotation2d.fromDegrees(-164.0))))
+        AutoPoint.ofRed(new Pose2d(13.24, 7.4, Rotation2d.kCW_90deg)),
+        AutoPoint.ofRed(new Pose2d(13.9, 5.92, Rotation2d.kCW_90deg)),
+        AutoPoint.ofRed(new Pose2d(14.716, 4.198, Rotation2d.kCW_90deg)))
           .untilFinished(new PoseErrorTolerance(0.05, 3));
 
   public TrenchRShootClimb2Auto(RobotManager robotManager, Trailblazer trailblazer) {
@@ -61,25 +66,36 @@ public class TrenchRShootClimb2Auto extends BaseImperativeAuto<TrenchRShootClimb
 
   @Override
   public Point getStartingPoint() {
-    return Point.ofRed(new Pose2d(12.2, 7.4, Rotation2d.kZero));
+    return Point.ofRed(new Pose2d(12.0, 7.4, Rotation2d.kCW_90deg));
   }
 
   @Override
   protected TrenchRShootClimb2AutoState getNextState(TrenchRShootClimb2AutoState currentState) {
     if (trailblazer.atGoal(robotManager.localization.getPose())) {
-      return switch (currentState) {
-        case DRIVE_TO_MIDLINE_1 -> TrenchRShootClimb2AutoState.INTAKE_ACROSS_MIDLINE_1;
-        case INTAKE_ACROSS_MIDLINE_1 -> TrenchRShootClimb2AutoState.DRIVE_BACK_1;
-        case DRIVE_BACK_1 -> TrenchRShootClimb2AutoState.SHOOT_1;
-        case SHOOT_1 -> TrenchRShootClimb2AutoState.DRIVE_TO_MIDLINE_2;
-        case DRIVE_TO_MIDLINE_2 -> TrenchRShootClimb2AutoState.INTAKE_ACROSS_MIDLINE_2;
-        case INTAKE_ACROSS_MIDLINE_2 -> TrenchRShootClimb2AutoState.DRIVE_BACK_2;
-        case DRIVE_BACK_2 -> TrenchRShootClimb2AutoState.SHOOT_2;
-        case SHOOT_2 -> TrenchRShootClimb2AutoState.DRIVE_TO_CLIMB;
-        case DRIVE_TO_CLIMB -> TrenchRShootClimb2AutoState.CLIMB;
-        case CLIMB -> TrenchRShootClimb2AutoState.DONE;
-        case DONE -> TrenchRShootClimb2AutoState.DONE;
-      };
+      switch (currentState) {
+        case DRIVE_TO_MIDLINE_1:
+          return TrenchRShootClimb2AutoState.INTAKE_ACROSS_MIDLINE_1;
+        case INTAKE_ACROSS_MIDLINE_1:
+          return TrenchRShootClimb2AutoState.DRIVE_BACK_1;
+        case DRIVE_BACK_1:
+          return TrenchRShootClimb2AutoState.SHOOT_1;
+        case SHOOT_1:
+          return timeout(3.0) ? TrenchRShootClimb2AutoState.DRIVE_TO_MIDLINE_2 : currentState;
+        case DRIVE_TO_MIDLINE_2:
+          return TrenchRShootClimb2AutoState.INTAKE_ACROSS_MIDLINE_2;
+        case INTAKE_ACROSS_MIDLINE_2:
+          return TrenchRShootClimb2AutoState.DRIVE_BACK_2;
+        case DRIVE_BACK_2:
+          return TrenchRShootClimb2AutoState.SHOOT_2;
+        case SHOOT_2:
+          return TrenchRShootClimb2AutoState.DRIVE_TO_CLIMB;
+        case DRIVE_TO_CLIMB:
+          return TrenchRShootClimb2AutoState.CLIMB;
+        case CLIMB:
+          return TrenchRShootClimb2AutoState.DONE;
+        case DONE:
+          return TrenchRShootClimb2AutoState.DONE;
+      }
     }
     return currentState;
   }
@@ -96,7 +112,10 @@ public class TrenchRShootClimb2Auto extends BaseImperativeAuto<TrenchRShootClimb
         trailblazer.setActiveSegment(driveBack);
         robotManager.idleRequest();
       }
-      case SHOOT_1 -> robotManager.prepareScoreRequest();
+      case SHOOT_1 -> {
+        trailblazer.setActiveSegment(driveToShootOne);
+        robotManager.prepareScoreRequest();
+      }
       case DRIVE_TO_MIDLINE_2 -> trailblazer.setActiveSegment(driveToMidline);
       case INTAKE_ACROSS_MIDLINE_2 -> {
         trailblazer.setActiveSegment(intakeAcrossMidline);
@@ -106,7 +125,10 @@ public class TrenchRShootClimb2Auto extends BaseImperativeAuto<TrenchRShootClimb
         trailblazer.setActiveSegment(driveBack);
         robotManager.idleRequest();
       }
-      case SHOOT_2 -> robotManager.prepareScoreRequest();
+      case SHOOT_2 -> {
+        trailblazer.setActiveSegment(driveToShootTwo);
+        robotManager.prepareScoreRequest();
+      }
       case DRIVE_TO_CLIMB -> {
         trailblazer.setActiveSegment(driveToClimb);
         // raise climber as you shoot & drive to climb?
