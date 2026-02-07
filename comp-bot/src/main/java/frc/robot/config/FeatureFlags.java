1bfc78480e8e140ad7ffffa576c1461a54f078c3
Simon <87281874+simonp17@users.noreply.github.com>
2026-02-07 11:02:29 -0800

    Add feature flag for intake snaps

--- comp-bot/src/main/java/frc/robot/config/FeatureFlags.java
+++ comp-bot/src/main/java/frc/robot/config/FeatureFlags.java
@@ -10,6 +10,10 @@ public class FeatureFlags {
   public static final BooleanSupplier BUMP_ASSIST = FeatureFlag.of("BumpAssist", true);
   public static final BooleanSupplier TRENCH_ASSIST = FeatureFlag.of("TrenchAssist", true);
   public static final BooleanSupplier HOPPER_SHUFFLING = FeatureFlag.of("HopperShuffling", false);
+  public static final BooleanSupplier INTAKE_DIRECTIONAL_SNAPS =
+      FeatureFlag.of("IntakeDirectionalSnaps", false);
+
+  public static final BooleanSupplier INTAKE_WALL_SNAPS = FeatureFlag.of("IntakeWallSnaps", false);
 
   public static final BooleanSupplier REGRESSION_MODEL = FeatureFlag.of("RegressionModel", false);
 
