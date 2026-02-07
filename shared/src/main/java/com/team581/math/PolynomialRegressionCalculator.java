bb6eedb55d55323f5108f78c80191137b19e4fdc
Simon <87281874+simonp17@users.noreply.github.com>
2026-02-06 21:45:12 -0800

    Update regression calculator to get constants in constructor

--- shared/src/main/java/com/team581/math/PolynomialRegressionCalculator.java
+++ shared/src/main/java/com/team581/math/PolynomialRegressionCalculator.java
@@ -2,8 +2,17 @@ package com.team581.math;
 
 public class PolynomialRegressionCalculator {
 
-  public static double polynomialRegression(
-      double x, double leadingCoefficient, double slope, double yInt) {
+  private double leadingCoefficient;
+  private double slope;
+  private double yInt;
+
+  public PolynomialRegressionCalculator(double leadingCoefficient, double slope, double yInt) {
+    this.leadingCoefficient = leadingCoefficient;
+    this.slope = slope;
+    this.yInt = yInt;
+  }
+
+  public double calculate(double x) {
     return (leadingCoefficient * (x * x)) + (slope * x) + yInt;
   }
 }
