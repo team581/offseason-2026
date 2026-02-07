b1bdcb923956d285b7876f2624824e22e5563867
Owen <117956892+Owen756@users.noreply.github.com>
2026-02-07 11:49:10 -0800

    add health manager logs

--- comp-bot/src/main/java/frc/robot/health/HealthManager.java
+++ comp-bot/src/main/java/frc/robot/health/HealthManager.java
@@ -2,6 +2,7 @@ package frc.robot.health;
 
 import com.team581.mechanisms.vision.CameraHealth;
 import com.team581.util.state_machines.StateMachineSubsystem;
+import dev.doglog.DogLog;
 import edu.wpi.first.wpilibj.RobotBase;
 import frc.robot.config.DSOptions;
 import frc.robot.util.scheduling.SubsystemPriority;
@@ -44,5 +45,12 @@ public class HealthManager extends StateMachineSubsystem<HealthState> {
     fuelDetectionHealthy =
         RobotBase.isSimulation() || backLimelight.getCameraHealth() != CameraHealth.OFFLINE;
     allCamerasHealthy = localizationHealthy && fuelDetectionHealthy;
+  }
+
+  @Override
+  protected void whileInState(HealthState state) {
+    DogLog.log("Health/LocalizationHealthy", localizationHealthy);
+    DogLog.log("Health/FuelDetectionHealthy", fuelDetectionHealthy);
+    DogLog.log("Health/AllCamerasHealthy", allCamerasHealthy);
   }
 }
