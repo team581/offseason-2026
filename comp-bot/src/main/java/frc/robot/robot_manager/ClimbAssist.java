6ad1b5767c24cd35c92f03ede319f9b1a240b453
Simon <87281874+simonp17@users.noreply.github.com>
2026-02-07 21:03:14 -0800

    Tune climb assist with best guess tolerances

--- comp-bot/src/main/java/frc/robot/robot_manager/ClimbAssist.java
+++ comp-bot/src/main/java/frc/robot/robot_manager/ClimbAssist.java
@@ -4,6 +4,7 @@ import com.team581.math.PoseErrorTolerance;
 import com.team581.trailblazer.AutoPoint;
 import com.team581.trailblazer.Trailblazer;
 import com.team581.trailblazer.segments.AutoSegment;
+
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform2d;
@@ -11,13 +12,13 @@ import edu.wpi.first.math.util.Units;
 import frc.robot.climber.ClimbLocation;
 
 public class ClimbAssist {
-  private static final Transform2d APPROACH_TRANSFORM = new Transform2d(1.5, 0.0, Rotation2d.kZero);
+  private static final Transform2d APPROACH_TRANSFORM = new Transform2d(0.5, 0.0, Rotation2d.kZero);
 
   private static final PoseErrorTolerance APPROACH_TOLERANCE =
-      new PoseErrorTolerance(Units.inchesToMeters(10.0), Rotation2d.fromDegrees(5));
+      new PoseErrorTolerance(Units.inchesToMeters(5.0), Rotation2d.fromDegrees(5));
 
-  private static final double APPROACH_MAX_LINEAR_VELOCITY = 2.0;
-  private static final double MAX_LINEAR_ACCELERATION = 2.0;
+  private static final double MAX_LINEAR_VELOCITY = 2.0;
+  private static final double MAX_LINEAR_ACCELERATION = 1.0;
 
   public static AutoSegment getClimbAssistSegment(Pose2d robot, ClimbLocation location) {
     var goalPoint = location.getEndGoalPoint(robot);
@@ -25,7 +26,7 @@ public class ClimbAssist {
         goalPoint.transformBy(APPROACH_TRANSFORM).withTransitionTolerance(APPROACH_TOLERANCE);
 
     return Trailblazer.segment(approachPoint, AutoPoint.of(goalPoint))
-        .withLinearConstraints(APPROACH_MAX_LINEAR_VELOCITY, MAX_LINEAR_ACCELERATION)
+        .withLinearConstraints(MAX_LINEAR_VELOCITY, MAX_LINEAR_ACCELERATION)
         .forever();
   }
 }
