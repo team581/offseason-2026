76eace172975f6a8b23769a0e8aff911d5fb6729
Simon <87281874+simonp17@users.noreply.github.com>
2026-02-07 20:56:26 -0800

    Update climber config to best guess

--- comp-bot/src/main/java/frc/robot/climber/ClimberConfig.java
+++ comp-bot/src/main/java/frc/robot/climber/ClimberConfig.java
@@ -7,6 +7,7 @@ import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import com.team581.autos.Point;
+
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 
@@ -21,9 +22,9 @@ public class ClimberConfig {
 
   // TODO: UPDATE WITH REAL GOAL LOCATIONS FROM CAD
   public static final Point CLIMB_LEFT_LOCATION =
-      Point.ofRed(new Pose2d(15.0, 1.0, Rotation2d.kCW_90deg));
+      Point.ofRed(new Pose2d(14.968, 3.867, Rotation2d.k180deg));
   public static final Point CLIMB_RIGHT_LOCATION =
-      Point.ofRed(new Pose2d(15.0, 5.5, Rotation2d.kCCW_90deg));
+      Point.ofRed(new Pose2d(14.968, 4.748, Rotation2d.k180deg));
 
   public static final double TOLERANCE = 0.1;
 }
