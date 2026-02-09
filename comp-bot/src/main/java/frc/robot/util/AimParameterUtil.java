e31e72292b2e115e8efeddaadb58be80dff0afd7
Jordan <howlinheat555@gmail.com>
2026-02-08 20:00:10 -0800

    Revert unpolished shoot on the move logic

--- comp-bot/src/main/java/frc/robot/util/AimParameterUtil.java
+++ comp-bot/src/main/java/frc/robot/util/AimParameterUtil.java
@@ -3,7 +3,7 @@ package frc.robot.util;
 import com.team581.math.ShootOnTheMove;
 import com.team581.util.FeedLocation;
 import com.team581.util.FieldUtil;
-import dev.doglog.DogLog;
+
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import frc.robot.shooter.ShooterConfig;
@@ -14,57 +14,30 @@ public class AimParameterUtil {
       new ShootOnTheMove(ShooterConfig.DISTANCE_TO_FEED_TOF);
   private static final ShootOnTheMove SCORING_SOTM =
       new ShootOnTheMove(ShooterConfig.DISTANCE_TO_SCORE_TOF);
-
   public static AimingParameters getFeedingParameters(
       FeedLocation feedLocation, Pose2d robot, ChassisSpeeds fieldRelativeSpeeds) {
     var feedTranslation =
         FEEDING_SOTM.getVelocityCompensatedGoal(
             robot.getTranslation(), feedLocation.getTranslation(robot), fieldRelativeSpeeds);
-
     var turretAngle = TurretCalculator.calculateTurretAimingAngle(robot, feedTranslation);
     var distanceToGoal = robot.getTranslation().getDistance(feedTranslation);
-
     return new AimingParameters(turretAngle, distanceToGoal);
   }
 
   public static AimingParameters getScoringParameters(
       Pose2d robot, ChassisSpeeds fieldRelativeSpeeds) {
-    var hubTranslationTangential =
-        SCORING_SOTM.getTangentialVelocityCompensatedGoal(
-            robot.getTranslation(),
-            FieldUtil.HUB_POSE.getPose().getTranslation(),
-            fieldRelativeSpeeds);
-
-    var hubTranslationRadial =
-        SCORING_SOTM.getRadialVelocityCompensatedGoal(
+    var hubTranslation =
+        SCORING_SOTM.getVelocityCompensatedGoal(
             robot.getTranslation(),
             FieldUtil.HUB_POSE.getPose().getTranslation(),
             fieldRelativeSpeeds);
 
     var robotPoseInAllianceZone = FieldUtil.clampPoseToAllianceZone(robot);
     double turretAngle =
-        TurretCalculator.calculateTurretAimingAngle(
-            robotPoseInAllianceZone, hubTranslationTangential);
-    double distanceToGoal =
-        robotPoseInAllianceZone.getTranslation().getDistance(hubTranslationRadial);
-
-    DogLog.log(
-        "AimParameterUtil/RadialHubTranslation",
-        new Pose2d(
-            hubTranslationRadial.getX(),
-            hubTranslationRadial.getY(),
-            hubTranslationRadial.getAngle()));
-    DogLog.log(
-        "AimParameterUtil/TangentialHubTranslation",
-        new Pose2d(
-            hubTranslationTangential.getX(),
-            hubTranslationTangential.getY(),
-            hubTranslationTangential.getAngle()));
-    DogLog.log("AimParameterUtil/DistanceToGoal", distanceToGoal);
-    DogLog.log("AimParameterUtil/TurretAngle", turretAngle);
+        TurretCalculator.calculateTurretAimingAngle(robotPoseInAllianceZone, hubTranslation);
+    double distanceToGoal = robotPoseInAllianceZone.getTranslation().getDistance(hubTranslation);
 
     return new AimingParameters(turretAngle, distanceToGoal);
   }
-
   public record AimingParameters(double turretAngle, double distance) {}
 }
--- shared/src/main/java/com/team581/math/ShootOnTheMove.java
+++ shared/src/main/java/com/team581/math/ShootOnTheMove.java
@@ -1,8 +1,5 @@
 package com.team581.math;
 
-import dev.doglog.DogLog;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
@@ -13,57 +10,6 @@ public class ShootOnTheMove {
 
   public ShootOnTheMove(InterpolatingDoubleTreeMap distanceToTimeOfFlight) {
     this.distanceToTimeOfFlight = distanceToTimeOfFlight;
-  }
-
-  public Translation2d getRadialVelocityCompensatedGoal(
-      Translation2d robot, Translation2d target, ChassisSpeeds robotVelocity) {
-    var result = target;
-
-    for (int i = 0; i < MAX_ITERATIONS; i++) {
-      double tof = distanceToTimeOfFlight.get(robot.getDistance(target));
-
-      // 1. Get the direction from robot to target as a Rotation2d
-      Rotation2d angleToTarget = target.minus(robot).getAngle();
-
-      // 2. Extract the radial velocity using trigonometry
-      double radialVelocityMagnitude =
-          (robotVelocity.vxMetersPerSecond * angleToTarget.getCos())
-              + (robotVelocity.vyMetersPerSecond * angleToTarget.getSin());
-
-      // 3. Create the offset vector and subtract it
-      Translation2d offset = new Translation2d(radialVelocityMagnitude * tof, angleToTarget);
-      result = target.minus(offset);
-    }
-    DogLog.log(
-        "ShootOnTheMove/RadialVelocityCompensatedGoal", new Pose2d(result, result.getAngle()));
-    return result;
-  }
-
-  public Translation2d getTangentialVelocityCompensatedGoal(
-      Translation2d robot, Translation2d target, ChassisSpeeds robotVelocity) {
-    var result = target;
-
-    for (int i = 0; i < MAX_ITERATIONS; i++) {
-      double tof = distanceToTimeOfFlight.get(robot.getDistance(target));
-
-      // 1. Get the radial vector (robot to target)
-      Translation2d radialDirection = target.minus(robot).div(robot.getDistance(target));
-
-      // 2. Get the tangential vector (rotate radial by 90 degrees)
-      Translation2d tangentialDirection =
-          new Translation2d(-radialDirection.getY(), radialDirection.getX());
-
-      // 3. Project robot velocity onto the tangential vector
-      double tangentialVelocityMagnitude =
-          (robotVelocity.vxMetersPerSecond * tangentialDirection.getX())
-              + (robotVelocity.vyMetersPerSecond * tangentialDirection.getY());
-
-      // 4. Offset target sideways
-      result = target.minus(tangentialDirection.times(tangentialVelocityMagnitude * tof));
-    }
-    DogLog.log(
-        "ShootOnTheMove/TangentialVelocityCompensatedGoal", new Pose2d(result, result.getAngle()));
-    return result;
   }
 
   public Translation2d getVelocityCompensatedGoal(
