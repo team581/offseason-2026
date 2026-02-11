4155bab2c01e24f55b6f5e94e125a0bb2aaba667
Simon <87281874+simonp17@users.noreply.github.com>
2026-02-10 18:50:40 -0800

    Add correct bus in hardware

--- comp-bot/src/main/java/frc/robot/Hardware.java
+++ comp-bot/src/main/java/frc/robot/Hardware.java
@@ -1,5 +1,6 @@
 package frc.robot;
 
+import com.ctre.phoenix6.CANBus;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.CANdle;
 import com.ctre.phoenix6.hardware.CANrange;
@@ -14,23 +15,26 @@ public class Hardware {
   public final XboxController driverController = new XboxController(0);
   public final XboxController operatorController = new XboxController(1);
 
-  public final TalonFX shooterLeftMotor = new TalonFX(15);
-  public final TalonFX shooterRightMotor = new TalonFX(16);
-  public final TalonFX turretMotor = new TalonFX(17);
-  public final TalonFX rotorMotor = new TalonFX(18);
-  public final TalonFX horizontalMotor = new TalonFX(19);
-  public final TalonFX verticalMotor = new TalonFX(20);
-  public final TalonFX intakeMotor = new TalonFX(21);
-  public final TalonFX leftDeployMotor = new TalonFX(22);
-  public final TalonFX rightDeployMotor = new TalonFX(23);
-  public final TalonFX shooterHoodMotor = new TalonFX(24);
-  public final TalonFX climbMotor = new TalonFX(25);
+    private final CANBus canivore = new CANBus("581CANivore");
+    private final CANBus rio = new CANBus();
 
-  public final CANcoder turretEncoder = new CANcoder(26);
+  public final TalonFX shooterLeftMotor = new TalonFX(15, rio);
+  public final TalonFX shooterRightMotor = new TalonFX(16, rio);
+  public final TalonFX turretMotor = new TalonFX(17, canivore);
+  public final TalonFX rotorMotor = new TalonFX(18, canivore);
+  public final TalonFX horizontalMotor = new TalonFX(19, rio);
+  public final TalonFX verticalMotor = new TalonFX(20, rio);
+  public final TalonFX intakeMotor = new TalonFX(21, rio);
+  public final TalonFX leftDeployMotor = new TalonFX(22, canivore);
+  public final TalonFX rightDeployMotor = new TalonFX(23, canivore);
+  public final TalonFX shooterHoodMotor = new TalonFX(24, rio);
+  public final TalonFX climbMotor = new TalonFX(25, canivore);
 
-  public final CANdle candle = new CANdle(27);
+  public final CANcoder turretEncoder = new CANcoder(26, canivore);
 
-  public final CANrange hopperCANRange = new CANrange(28);
+  public final CANdle candle = new CANdle(27, rio);
+
+  public final CANrange hopperCANRange = new CANrange(28, canivore);
 
   public final TunerSwerveDrivetrain drivetrain =
       RobotKind.IS_COMP_BOT
