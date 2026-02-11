e5f32d624b85cf8a8fdf8b1765329737536c70d4
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2026-02-10 18:31:06 -0800

    remove todo

--- shared/src/main/java/com/team581/autos/Point.java
+++ shared/src/main/java/com/team581/autos/Point.java
@@ -16,7 +16,6 @@ public record Point(Pose2d redPose, Pose2d bluePose) {
   public static final BooleanSupplier CLAMPED_POINTS_FEATURE_FLAG =
       FeatureFlag.of("ClampedAutoPoints", true);
 
-  // TODO(@fcuellar13): Update the clamped area to reflect home practice field
   private static final Rectangle2d CLAMPED_AREA =
       new Rectangle2d(new Translation2d(16.54, 8.069), new Translation2d(8.25, 1.649));
 
