cf16ec3bcebfc8910a067693dd0f14242e9bd1d0
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2026-02-12 20:01:49 -0800

    update auto selection again

--- comp-bot/src/main/java/frc/robot/autos/AutoSelection.java
+++ comp-bot/src/main/java/frc/robot/autos/AutoSelection.java
@@ -4,16 +4,18 @@ import com.team581.autos.AutoSelectionBase;
 import com.team581.trailblazer.Trailblazer;
 import frc.robot.autos.auto_state_machines.DoNothingAuto;
 import frc.robot.autos.auto_state_machines.IntegrationTest;
-// import frc.robot.autos.auto_state_machines.TrenchRFeed2Auto;
+import frc.robot.autos.auto_state_machines.RightStraightFeedAuto;
 import frc.robot.autos.auto_state_machines.RightStraightShootClimbAuto;
+import frc.robot.autos.auto_state_machines.RightSwoopShootClimbAuto;
 import frc.robot.robot_manager.RobotManager;
 import java.util.function.BiFunction;
 
 public enum AutoSelection implements AutoSelectionBase {
   DO_NOTHING(DoNothingAuto::new),
-  // TRENCH_FEED_2(TrenchRFeed2Auto::new),
+  RIGHT_STRAIGHT_FEED(RightStraightFeedAuto::new),
   RIGHT_STRAIGHT_SHOOT_CLIMB(RightStraightShootClimbAuto::new),
-  // RIGHT_SWOOP_SHOOT_CLIMB(),
+  
+  RIGHT_SWOOP_SHOOT_CLIMB(RightSwoopShootClimbAuto::new),
   INTEGRATION_TEST(IntegrationTest::new);
 
   public final BiFunction<RobotManager, Trailblazer, BaseImperativeAuto<?>> auto;
