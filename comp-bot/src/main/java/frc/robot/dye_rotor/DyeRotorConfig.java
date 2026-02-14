feaff71b07311872c9c91d311b3c02a39a2d1abb
Owen <117956892+Owen756@users.noreply.github.com>
2026-02-14 13:30:29 -0800

    restore kv to 5 for dye rotor

--- comp-bot/src/main/java/frc/robot/dye_rotor/DyeRotorConfig.java
+++ comp-bot/src/main/java/frc/robot/dye_rotor/DyeRotorConfig.java
@@ -39,8 +39,7 @@ public class DyeRotorConfig {
               new MotorOutputConfigs()
                   .withNeutralMode(NeutralModeValue.Coast)
                   .withInverted(InvertedValue.Clockwise_Positive))
-          // TODO: Restore kV of 5.0 once bringup is finished
-          .withSlot0(new Slot0Configs().withKP(0.0).withKV(0.0).withKS(0.0).withKA(0.0));
+          .withSlot0(new Slot0Configs().withKP(0.0).withKV(5.0).withKS(0.0).withKA(0.0));
 
   public static final TalonFXConfiguration VERTICAL_MOTOR_CONFIG =
       new TalonFXConfiguration()
