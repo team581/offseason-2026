46fd40cb9ad073bcd9c38567928c98f16e2b2000
Simon <87281874+simonp17@users.noreply.github.com>
2026-02-15 11:31:22 -0800

    Imrpove DSOptions for comp bot

--- comp-bot/src/main/java/frc/robot/config/DSOptions.java
+++ comp-bot/src/main/java/frc/robot/config/DSOptions.java
@@ -4,10 +4,8 @@ import com.team581.config.DSOption;
 import edu.wpi.first.networktables.BooleanSubscriber;
 
 public final class DSOptions {
-  public static final BooleanSubscriber SENSOR_OVERRIDE = DSOption.of("SensorOverride", false);
   public static final BooleanSubscriber USE_TAG_LIMELIGHTS = DSOption.of("UseTagLimelights", true);
-  public static final BooleanSubscriber bypassHubStateTracking =
-      DSOption.of("BypassHubStateTracking", false);
+  public static final BooleanSubscriber USE_HUB_STATE = DSOption.of("UseHubState", false);
 
   private DSOptions() {}
 }
