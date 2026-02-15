89b38506790e7ac2e27848e5837a5f9f6194a554
Jordan <117786700+jordanjcoderman@users.noreply.github.com>
2026-02-14 16:40:35 -0800

    If we are scoring and intaking, resume hopper shuffling after we stop intaking

--- comp-bot/src/main/java/frc/robot/robot_manager/RobotManager.java
+++ comp-bot/src/main/java/frc/robot/robot_manager/RobotManager.java
@@ -880,6 +880,23 @@ public class RobotManager extends StateMachineSubsystem<RobotState> {
 
   public void cancelIntakeRequest() {
     intake.idleRequest();
+
+    // If we are shooting while cancelling a previous intake request, send a new hopper shuffle
+    // request
+    switch (getState()) {
+      case PREPARE_FORCE_SCORE,
+          FORCE_SCORE,
+          PREPARE_PRESET_SCORE,
+          PRESET_SCORE,
+          PREPARE_SCORE,
+          SCORE,
+          PREPARE_PRESET_FEED,
+          PRESET_FEED,
+          PREPARE_FEED,
+          FEED ->
+          deploy.shuffleRequest();
+      default -> {}
+    }
   }
 
   public void stowDeployRequest() {
