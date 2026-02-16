d3552c57937acbf2c20f419572b8984eb739b2b3
Jordan <howlinheat555@gmail.com>
2026-02-16 15:29:05 -0800

    Check if we are already scoring/feeding before a score/feed request

--- comp-bot/src/main/java/frc/robot/robot_manager/RobotManager.java
+++ comp-bot/src/main/java/frc/robot/robot_manager/RobotManager.java
@@ -944,13 +944,13 @@ public class RobotManager extends StateMachineSubsystem<RobotState> {
   }
 
   public void forceShootRequest() {
-    if (!getState().isClimbing()) {
+    if (!getState().isClimbing()  && getState() != RobotState.FORCE_SCORE) {
       setStateFromRequest(RobotState.PREPARE_FORCE_SCORE);
     }
   }
 
   public void prepareScoreRequest() {
-    if (!getState().isClimbing()) {
+    if (!getState().isClimbing() && getState() != RobotState.PRESET_SCORE && getState() != RobotState.SCORE) {
       if (!health.isLocalizationHealthy()) {
         setStateFromRequest(RobotState.PREPARE_PRESET_SCORE);
       } else {
@@ -960,7 +960,7 @@ public class RobotManager extends StateMachineSubsystem<RobotState> {
   }
 
   public void prepareFeedRequest() {
-    if (!getState().isClimbing()) {
+    if (!getState().isClimbing() && getState() != RobotState.PRESET_FEED  && getState() != RobotState.FEED) {
       if (!health.isLocalizationHealthy()) {
         setStateFromRequest(RobotState.PREPARE_PRESET_FEED);
       } else {
