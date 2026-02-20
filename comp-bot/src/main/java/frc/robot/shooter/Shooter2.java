f5d848930cf83bdf179eda3bedaf5ca6c2fbf18c
Jordan <117786700+jordanjcoderman@users.noreply.github.com>
2026-02-19 19:01:10 -0800

    Shooter 2 subsystem self test code fixed states, added values to Config and working on the subsystem itself

--- comp-bot/src/main/java/frc/robot/shooter/Shooter2.java
+++ comp-bot/src/main/java/frc/robot/shooter/Shooter2.java
@@ -11,7 +11,7 @@ import edu.wpi.first.math.MathUtil;
 import frc.robot.config.FeatureFlags;
 import frc.robot.util.scheduling.SubsystemPriority;
 
-public class Shooter2 extends StateMachineSubsystem<ShooterState> {
+public class Shooter2 extends StateMachineSubsystem<ShooterState2> {
   private static double distanceToScoringRpm(double distance) {
     return FeatureFlags.REGRESSION_MODEL.getAsBoolean()
         ? Shooter2Config.SCORING_REGRESSION_MODEL.calculate(distance)
@@ -27,20 +27,18 @@ public class Shooter2 extends StateMachineSubsystem<ShooterState> {
   private final TalonFX topMotor;
   private final TalonFX bottomMotor;
 
-  private final VelocityVoltage voltageRequest =
+  private final VelocityVoltage velocityRequest =
       new VelocityVoltage(0).withLimitReverseMotion(true).withEnableFOC(true);
-
   private double scoreDistance = 0;
   private double climbScoreRpm = 0;
   private double feedDistance = 0;
-
   private double shootingRpm = 0;
   private double feedingRpm = 0;
   private double topMotorRpm = 0;
   private double bottomMotorRpm = 0;
 
   public Shooter2(TalonFX topMotor, TalonFX bottomMotor) {
-    super(SubsystemPriority.SHOOTER, ShooterState.IDLE);
+    super(SubsystemPriority.SHOOTER, ShooterState2.IDLE);
 
     topMotor.getConfigurator().apply(Shooter2Config.TOP_MOTOR_CONFIGS);
     bottomMotor.getConfigurator().apply(Shooter2Config.BOTTOM_MOTOR_CONFIG);
@@ -52,27 +50,31 @@ public class Shooter2 extends StateMachineSubsystem<ShooterState> {
     this.bottomMotor = bottomMotor;
   }
 
+  public void startSelfTestRequest() {
+    setStateFromRequest(ShooterState2.SELF_TEST_STOP_MOTORS);
+  }
+
   public void scoreRequest(double distance) {
     this.scoreDistance = distance;
-    setStateFromRequest(ShooterState.SCORE);
+    setStateFromRequest(ShooterState2.SCORE);
   }
 
   public void climbScoreRequest(boolean isTop) {
     climbScoreRpm = 0.0;
-    setStateFromRequest(ShooterState.CLIMB_SCORE);
+    setStateFromRequest(ShooterState2.CLIMB_SCORE);
   }
 
   public void feedRequest(double distance) {
     this.feedDistance = distance;
-    setStateFromRequest(ShooterState.FEEDING);
+    setStateFromRequest(ShooterState2.FEEDING);
   }
 
   public void idleRequest() {
-    setStateFromRequest(ShooterState.IDLE);
+    setStateFromRequest(ShooterState2.IDLE);
   }
 
   @Override
-  protected void whileInState(ShooterState state) {
+  protected void whileInState(ShooterState2 state) {
     DogLog.log("Shooter/Top/RPM", topMotorRpm);
     DogLog.log("Shooter/Bottom/RPM", bottomMotorRpm);
     DogLog.log("Shooter/GoalShootingRPM", shootingRpm);
@@ -89,30 +91,40 @@ public class Shooter2 extends StateMachineSubsystem<ShooterState> {
     switch (state) {
       case SCORE -> {
         var setpoint = shootingRpm / 60.0;
-        topMotor.setControl(voltageRequest.withVelocity(setpoint));
-        bottomMotor.setControl(voltageRequest.withVelocity(setpoint));
+        topMotor.setControl(velocityRequest.withVelocity(setpoint));
+        bottomMotor.setControl(velocityRequest.withVelocity(setpoint));
 
         DogLog.log("Shooter/RpmSetpoint", shootingRpm);
       }
       case CLIMB_SCORE -> {
         var setpoint = climbScoreRpm / 60.0;
-        topMotor.setControl(voltageRequest.withVelocity(setpoint));
-        bottomMotor.setControl(voltageRequest.withVelocity(setpoint));
+        topMotor.setControl(velocityRequest.withVelocity(setpoint));
+        bottomMotor.setControl(velocityRequest.withVelocity(setpoint));
 
         DogLog.log("Shooter/RpmSetpoint", climbScoreRpm);
       }
       case FEEDING -> {
         var setpoint = feedingRpm / 60.0;
-        topMotor.setControl(voltageRequest.withVelocity(setpoint));
-        bottomMotor.setControl(voltageRequest.withVelocity(setpoint));
+        topMotor.setControl(velocityRequest.withVelocity(setpoint));
+        bottomMotor.setControl(velocityRequest.withVelocity(setpoint));
 
         DogLog.log("Shooter/RpmSetpoint", feedingRpm);
       }
       case IDLE -> {
         topMotor.disable();
         bottomMotor.disable();
-
-        DogLog.log("Shooter/RpmSetpoint", -1.0);
+      }
+      case SELF_TEST_STOP_MOTORS -> {
+        topMotor.stopMotor();
+        bottomMotor.stopMotor();
+      }
+      case SELF_TEST_BOTTOM_MOTOR -> {
+        bottomMotor.setVoltage(Shooter2Config.TEST_VOLTAGE);
+        topMotor.stopMotor();
+      }
+      case SELF_TEST_TOP_MOTOR -> {
+        topMotor.setVoltage(Shooter2Config.TEST_VOLTAGE);
+        bottomMotor.stopMotor();
       }
     }
   }
@@ -121,9 +133,18 @@ public class Shooter2 extends StateMachineSubsystem<ShooterState> {
   protected void collectInputs() {
     shootingRpm = Math.min(Shooter2Config.MAX_SAFE_RPM, distanceToScoringRpm(scoreDistance));
     feedingRpm = Math.min(Shooter2Config.MAX_SAFE_RPM, distanceToFeedingRpm(feedDistance));
-
     topMotorRpm = topMotor.getVelocity().getValueAsDouble() * 60.0;
     bottomMotorRpm = bottomMotor.getVelocity().getValueAsDouble() * 60.0;
+
+    if (getState() == ShooterState2.SELF_TEST_TOP_MOTOR) {
+      DogLog.log("Shooter/SelfTest/TopMotor/VelocityGood", MathUtil.isNear(Shooter2Config.SELF_TEST_TOP_MOTOR_EXPECTED_RPM, topMotorRpm, Shooter2Config.SELF_TEST_TOP_MOTOR_RPM_TOLERANCE));
+      DogLog.log("Shooter/SelfTest/TopMotor/CurrentGood", MathUtil.isNear(Shooter2Config.SELF_TEST_TOP_MOTOR_EXPECTED_CURRENT, topMotor.getStatorCurrent().getValueAsDouble(), Shooter2Config.SELF_TEST_TOP_MOTOR_CURRENT_TOLERANCE));
+    }
+
+    if (getState() == ShooterState2.SELF_TEST_BOTTOM_MOTOR) {
+       DogLog.log("Shooter/SelfTest/BottomMotor/VelocityGood", MathUtil.isNear(Shooter2Config.SELF_TEST_BOTTOM_MOTOR_EXPECTED_RPM, topMotorRpm, Shooter2Config.SELF_TEST_BOTTOM_MOTOR_RPM_TOLERANCE));
+      DogLog.log("Shooter/SelfTest/BottomMotor/CurrentGood", MathUtil.isNear(Shooter2Config.SELF_TEST_BOTTOM_MOTOR_EXPECTED_CURRENT, bottomMotor.getStatorCurrent().getValueAsDouble(), Shooter2Config.SELF_TEST_BOTTOM_MOTOR_CURRENT_TOLERANCE));
+    }
   }
 
   public boolean atGoal() {
@@ -140,6 +161,21 @@ public class Shooter2 extends StateMachineSubsystem<ShooterState> {
       case FEEDING ->
           MathUtil.isNear(topMotorRpm, feedingRpm, Shooter2Config.RPM_TOLERANCE_SHOOTER)
               && MathUtil.isNear(bottomMotorRpm, feedingRpm, Shooter2Config.RPM_TOLERANCE_SHOOTER);
+      default -> true;
+    };
+  }
+
+  @Override
+  protected ShooterState2 getNextState(ShooterState2 currentState) {
+    return switch (currentState) {
+      case SELF_TEST_TOP_MOTOR -> timeout(5) ? ShooterState2.SELF_TEST_STOP_MOTORS : currentState;
+      // TODO: This is currently looping between SELF_TEST_STOP_MOTORS and SELF_TEST_BOTTOM_MOTOR infinitely
+      case SELF_TEST_STOP_MOTORS ->
+          timeout(2) ? ShooterState2.SELF_TEST_BOTTOM_MOTOR : currentState;
+      case SELF_TEST_BOTTOM_MOTOR ->
+          timeout(5) ? ShooterState2.SELF_TEST_STOP_MOTORS : currentState;
+
+      default -> currentState;
     };
   }
 
