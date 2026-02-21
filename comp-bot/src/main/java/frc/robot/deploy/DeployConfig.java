ced2f959e37ab208d4014ecc3b6e1740c851bf68
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2026-02-21 13:17:09 -0800

    new deploy supply current limits

--- comp-bot/src/main/java/frc/robot/deploy/DeployConfig.java
+++ comp-bot/src/main/java/frc/robot/deploy/DeployConfig.java
@@ -47,7 +47,7 @@ public class DeployConfig {
               new MotorOutputConfigs()
                   .withNeutralMode(NeutralModeValue.Coast)
                   .withInverted(InvertedValue.Clockwise_Positive))
-          .withCurrentLimits(new CurrentLimitsConfigs().withStatorCurrentLimit(30))
+          .withCurrentLimits(new CurrentLimitsConfigs().withStatorCurrentLimit(30).withSupplyCurrentLimit(18))
           .withMotionMagic(
               new MotionMagicConfigs()
                   .withMotionMagicCruiseVelocity(200.0)
@@ -63,7 +63,7 @@ public class DeployConfig {
               new MotorOutputConfigs()
                   .withNeutralMode(NeutralModeValue.Coast)
                   .withInverted(InvertedValue.CounterClockwise_Positive))
-          .withCurrentLimits(new CurrentLimitsConfigs().withStatorCurrentLimit(30))
+          .withCurrentLimits(new CurrentLimitsConfigs().withStatorCurrentLimit(30).withSupplyCurrentLimit(18))
           .withMotionMagic(
               new MotionMagicConfigs()
                   .withMotionMagicCruiseVelocity(200.0)
