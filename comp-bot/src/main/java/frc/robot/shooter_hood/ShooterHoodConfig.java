418fd36f25cf8bfbca3481503c45d1f313516643
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2026-02-21 15:53:52 -0800

    shooter hood ratio updated

--- comp-bot/src/main/java/frc/robot/shooter_hood/ShooterHoodConfig.java
+++ comp-bot/src/main/java/frc/robot/shooter_hood/ShooterHoodConfig.java
@@ -35,7 +35,7 @@ public class ShooterHoodConfig {
   public static final TalonFXConfiguration MOTOR_CONFIG =
       new TalonFXConfiguration()
           .withFeedback(
-              new FeedbackConfigs().withSensorToMechanismRatio((340.0 * 42.0) / (14.0 * 8.0)))
+              new FeedbackConfigs().withSensorToMechanismRatio((336.0 * 42.0) / (14.0 * 8.0)))
           .withCurrentLimits(
               new CurrentLimitsConfigs().withStatorCurrentLimit(20).withSupplyCurrentLimit(10))
           .withVoltage(new VoltageConfigs().withPeakForwardVoltage(10).withPeakReverseVoltage(-10))
