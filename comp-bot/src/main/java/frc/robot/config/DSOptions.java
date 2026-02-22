10eb353c25c6b1879d84aa2321bfa0aecf9161ff
Jordan <117786700+jordanjcoderman@users.noreply.github.com>
2026-02-21 16:59:55 -0800

    DSOption addition and swerve implementation

--- comp-bot/src/main/java/frc/robot/config/DSOptions.java
+++ comp-bot/src/main/java/frc/robot/config/DSOptions.java
@@ -6,9 +6,9 @@ import edu.wpi.first.networktables.BooleanSubscriber;
 public final class DSOptions {
   public static final BooleanSubscriber USE_TAG_LIMELIGHTS = DSOption.of("UseTagLimelights", true);
   public static final BooleanSubscriber USE_HUB_STATE = DSOption.of("UseHubState", false);
-  // TODO: This should be true when the CANrange is actually physically on the robot
+  public static final BooleanSubscriber USE_SWERVE_ASSIST = DSOption.of("UseSwerveAssist", false);
   public static final BooleanSubscriber USE_CANRANGE = DSOption.of("UseCANRange", false);
   public static final BooleanSubscriber USE_TURRET = DSOption.of("UseTurret", true);
-
+// TODO: This should be true when the CANrange is actually physically on the robot
   private DSOptions() {}
 }
