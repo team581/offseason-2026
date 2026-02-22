f0b164b49dfcc9e4efcb99fe92af9baffd16398f
Simon <87281874+simonp17@users.noreply.github.com>
2026-02-22 10:28:02 -0800

    Default to using stop shooting states

--- comp-bot/src/main/java/frc/robot/config/FeatureFlags.java
+++ comp-bot/src/main/java/frc/robot/config/FeatureFlags.java
@@ -29,7 +29,7 @@ public class FeatureFlags {
   public static final BooleanSupplier REGRESSION_MODEL = FeatureFlag.of("RegressionModel", false);
 
   public static final BooleanSupplier STOP_SHOOTING_STATE =
-      FeatureFlag.of("UseStopShootingState", false);
+      FeatureFlag.of("UseStopShootingState", true);
 
   public static final BooleanSupplier CANCEL_IN_PROGRESS_SHOT_RPM_DIP =
       FeatureFlag.of("StopScoringRPMDip", true);
