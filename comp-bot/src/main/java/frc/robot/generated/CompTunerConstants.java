bc09559b4fb159d2680ce32c47dba44e4304dc83
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2026-02-22 16:24:47 -0800

    add supply current limits to swerve drive

--- comp-bot/src/main/java/frc/robot/generated/CompTunerConstants.java
+++ comp-bot/src/main/java/frc/robot/generated/CompTunerConstants.java
@@ -77,7 +77,8 @@ public class CompTunerConstants {
 
     // Initial configs for the drive and steer motors and the azimuth encoder; these cannot be null.
     // Some configs will be overwritten; check the `with*InitialConfigs()` API documentation.
-    private static final TalonFXConfiguration driveInitialConfigs = new TalonFXConfiguration();
+    private static final TalonFXConfiguration driveInitialConfigs = new TalonFXConfiguration()
+    .withCurrentLimits(new CurrentLimitsConfigs().withSupplyCurrentLimit(Amps.of(60)));
     private static final TalonFXConfiguration steerInitialConfigs = new TalonFXConfiguration()
         .withCurrentLimits(
             new CurrentLimitsConfigs()
