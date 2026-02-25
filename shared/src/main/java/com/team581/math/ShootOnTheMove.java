3652f750037e3396d94b65267534690442e1ee9a
rhetorr <140564619+rhetorr@users.noreply.github.com>
2026-02-24 18:50:32 -0800

    add effective time of flight calculator

--- shared/src/main/java/com/team581/math/ShootOnTheMove.java
+++ shared/src/main/java/com/team581/math/ShootOnTheMove.java
@@ -7,9 +7,16 @@ import edu.wpi.first.math.kinematics.ChassisSpeeds;
 public class ShootOnTheMove {
   private static final int MAX_ITERATIONS = 5;
   private final InterpolatingDoubleTreeMap distanceToTimeOfFlight;
+  // TODO: find drag constant. Currently eyeballed
+  // https://frc-docs--3242.org.readthedocs.build/en/3242/docs/software/advanced-controls/fire-control/linear-drag.html
+  public static final double DRAG_CONSTANT = 0.9523;
 
   public ShootOnTheMove(InterpolatingDoubleTreeMap distanceToTimeOfFlight) {
     this.distanceToTimeOfFlight = distanceToTimeOfFlight;
+  }
+
+  public double getEffectiveTimeOfFlight(double tof) {
+    return (1-Math.pow(Math.E, (-ShootOnTheMove.DRAG_CONSTANT*tof)))/ShootOnTheMove.DRAG_CONSTANT;
   }
 
   public Translation2d getVelocityCompensatedGoal(
@@ -24,6 +31,25 @@ public class ShootOnTheMove {
           new Translation2d(
               target.getX() - (robotVelocity.vxMetersPerSecond * timeOfFlight),
               target.getY() - (robotVelocity.vyMetersPerSecond * timeOfFlight));
+    }
+
+    return result;
+  }
+
+  public Translation2d getVelocityCompensatedGoalWithEffectiveTof(
+      Translation2d robot, Translation2d target, ChassisSpeeds robotVelocity) {
+    var timeOfFlight = 0.0;
+    var effectiveTimeOfFlight = 0.0;
+    var result = target;
+
+    for (int i = 0; i < MAX_ITERATIONS; i++) {
+      timeOfFlight = distanceToTimeOfFlight.get(robot.getDistance(result));
+      effectiveTimeOfFlight = getEffectiveTimeOfFlight(timeOfFlight);
+      // Compensated goal = real goal - (robot velocity * time of flight of ball)
+      result =
+          new Translation2d(
+              target.getX() - (robotVelocity.vxMetersPerSecond * effectiveTimeOfFlight),
+              target.getY() - (robotVelocity.vyMetersPerSecond * effectiveTimeOfFlight));
     }
 
     return result;
