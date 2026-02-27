89e9268a399bdd3b71472b4c13b9b2f8b078907a
Simon <87281874+simonp17@users.noreply.github.com>
2026-02-26 20:32:29 -0800

    Run dye rotor unjam at -1 bps

--- comp-bot/src/main/java/frc/robot/dye_rotor/DyeRotorState.java
+++ comp-bot/src/main/java/frc/robot/dye_rotor/DyeRotorState.java
@@ -16,7 +16,7 @@ public enum DyeRotorState {
   FEED_CLEANUP_INTAKE_SCAN(5.0, -145.0, 10.0, 10.0),
   FEED_CLEANUP_WHIP_AROUND(20.0, 55.0, 10.0, 10.0),
 
-  UNJAM(0.0, 180.0, 0.0, 0.0);
+  UNJAM(-1.0, 180.0, 0.0, 0.0);
 
   private static double bpsToRpm(double bps) {
     var circumference = 18.0 * Math.PI;
