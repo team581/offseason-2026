3b6a90dbbb87b822ca440fa666ff1fe846afea4a
Simon <87281874+simonp17@users.noreply.github.com>
2026-02-28 07:45:28 -0800

    Fix FmsUtil tests

--- shared/src/test/java/com/team581/util/FmsUtilTest.java
+++ shared/src/test/java/com/team581/util/FmsUtilTest.java
@@ -9,27 +9,27 @@ final class FmsUtilTest {
   void hubIsActiveInAutoTest() {
     var time = 5.0;
     // 0.0 - 20.0
-    assertThat(FmsUtil.isHubActive(time)).isTrue();
+    assertThat(FmsUtil.isHubActive(time, true)).isTrue();
   }
 
   @Test
   void hubIsActiveInShift2Test() {
     var time = 60.0;
     // 55.0 - 80.0
-    assertThat(FmsUtil.isHubActive(time)).isTrue();
+    assertThat(FmsUtil.isHubActive(time, true)).isTrue();
   }
 
   @Test
   void hubIsActiveInTransitionShiftTest() {
     var time = 25.0;
     // 20.0 - 30.0
-    assertThat(FmsUtil.isHubActive(time)).isTrue();
+    assertThat(FmsUtil.isHubActive(time, true)).isTrue();
   }
 
   @Test
   void hubIsNotActiveInShift1Test() {
     var time = 35.0;
     // 35.0 - 55.0
-    assertThat(FmsUtil.isHubActive(time)).isFalse();
+    assertThat(FmsUtil.isHubActive(time, true)).isFalse();
   }
 }
