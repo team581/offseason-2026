f8569b84b6e899b70d71670551d892225d12975d
Jonah Snider <jonah@jonahsnider.com>
2026-03-04 13:49:00 -0800

    Add CameraConfig#getTransform3d()
    Signed-off-by: Jonah Snider <jonah@jonahsnider.com>

--- shared/src/main/java/com/team581/config/CameraConfig.java
+++ shared/src/main/java/com/team581/config/CameraConfig.java
@@ -1,5 +1,9 @@
 package com.team581.config;
 
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
+
 public record CameraConfig(
     LimelightModel model,
     boolean useMt2,
@@ -9,4 +13,11 @@ public record CameraConfig(
     double up,
     double pitch,
     double yaw,
-    double roll) {}
+    double roll) {
+
+  public Transform3d getTransform3d() {
+    return new Transform3d(
+        new Translation3d(forward, right, up),
+        new Rotation3d(Math.toRadians(roll), Math.toRadians(pitch), Math.toRadians(yaw)));
+  }
+}
