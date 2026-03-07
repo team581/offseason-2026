79b9ed4348556d5eea0d7bdea3f7adcfe14e5876
Jonah Snider <jonah@jonahsnider.com>
2026-03-07 11:10:30 -0800

    Add generic intake class
    Signed-off-by: Jonah Snider <jonah@jonahsnider.com>

---
+++ comp-bot/src/main/java/frc/robot/intake/GenericIntake.java
@@ -0,0 +1,22 @@
+package frc.robot.intake;
+
+import com.team581.util.state_machines.StateMachineSubsystem;
+import frc.robot.util.scheduling.SubsystemPriority;
+
+public abstract class GenericIntake extends StateMachineSubsystem<IntakeState> {
+  protected GenericIntake() {
+    super(SubsystemPriority.INTAKE, IntakeState.IDLE);
+  }
+
+  public abstract void idleRequest();
+
+  public abstract void intakeAutoRequest();
+
+  public abstract void intakeRequest();
+
+  public abstract void shootRequest();
+
+  public abstract void shootThenIntakeRequest();
+
+  public abstract void stopShootingRequest();
+}
