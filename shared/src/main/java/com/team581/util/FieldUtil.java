bbabe4d36b30309b61aef7cc8b173a0238c36744
Jordan <117786700+jordanjcoderman@users.noreply.github.com>
2026-03-10 18:47:53 -0700

    Increase obstruction zone tolerance to 12 inches

--- shared/src/main/java/com/team581/util/FieldUtil.java
+++ shared/src/main/java/com/team581/util/FieldUtil.java
@@ -409,7 +409,7 @@ public class FieldUtil {
 
   public static final ObstructionCalculator FEEDING_OBSTRUCTIONS =
       ObstructionCalculator.fromTranslations(
-          Units.inchesToMeters(6.0),
+          Units.inchesToMeters(12.0),
           BLUE_HUB_CORNERS,
           RED_HUB_CORNERS,
           BLUE_CLIMB_TOWER_CORNERS,
@@ -417,7 +417,7 @@ public class FieldUtil {
 
   public static final ObstructionCalculator HUB_SCORING_OBSTRUCTIONS =
       ObstructionCalculator.fromTranslations(
-          Units.inchesToMeters(6.0), BLUE_CLIMB_TOWER_CORNERS, RED_CLIMB_TOWER_CORNERS);
+          Units.inchesToMeters(12.0), BLUE_CLIMB_TOWER_CORNERS, RED_CLIMB_TOWER_CORNERS);
 
   public static Pose2d clampPoseToAllianceZone(Pose2d robot) {
     if (isRobotInAllianceZone(robot.getTranslation())) {
