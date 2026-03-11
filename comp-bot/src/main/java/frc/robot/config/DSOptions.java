b7ab44b37c403ac685059ffb6c352de8cadbceba
Simon <87281874+simonp17@users.noreply.github.com>
2026-03-11 10:41:13 -0700

    Default USE_HUB_STATE to true

--- comp-bot/src/main/java/frc/robot/config/DSOptions.java
+++ comp-bot/src/main/java/frc/robot/config/DSOptions.java
@@ -5,7 +5,7 @@ import edu.wpi.first.networktables.BooleanSubscriber;
 
 public final class DSOptions {
   public static final BooleanSubscriber USE_TAG_LIMELIGHTS = DSOption.of("UseTagLimelights", true);
-  public static final BooleanSubscriber USE_HUB_STATE = DSOption.of("UseHubState", false);
+  public static final BooleanSubscriber USE_HUB_STATE = DSOption.of("UseHubState", true);
   public static final BooleanSubscriber USE_SWERVE_ASSIST = DSOption.of("UseSwerveAssist", true);
   // TODO: This should be true when the CANrange is actually physically on the robot
   public static final BooleanSubscriber USE_CANRANGE = DSOption.of("UseCANRange", false);
