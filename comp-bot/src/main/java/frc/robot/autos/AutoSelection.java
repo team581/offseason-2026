629d747db586d949eaaa97e3ea1df41dca5b9c14
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2026-03-13 16:03:28 -0700

    new left side auto

--- comp-bot/src/main/java/frc/robot/autos/AutoSelection.java
+++ comp-bot/src/main/java/frc/robot/autos/AutoSelection.java
@@ -3,6 +3,7 @@ package frc.robot.autos;
 import com.team581.autos.AutoSelectionBase;
 import com.team581.trailblazer.Trailblazer;
 import frc.robot.autos.auto_state_machines.DoNothingAuto;
+import frc.robot.autos.auto_state_machines.LeftCircleSoMAuto;
 import frc.robot.autos.auto_state_machines.RightCircleSoMAuto;
 // import frc.robot.autos.auto_state_machines.RightPullSwoopShootAuto;
 import frc.robot.autos.auto_state_machines.RightStraightShootClimbAuto;
@@ -13,7 +14,8 @@ public enum AutoSelection implements AutoSelectionBase {
   DO_NOTHING(DoNothingAuto::new),
   RIGHT_STRAIGHT_SHOOT(RightStraightShootClimbAuto::new),
   //  RIGHT_PULL_SWOOP_SHOOT(RightPullSwoopShootAuto::new),
-  RIGHT_CIRCLE_SOM(RightCircleSoMAuto::new);
+  RIGHT_CIRCLE_SOM(RightCircleSoMAuto::new),
+  LEFT_CIRCLE_SOM(LeftCircleSoMAuto::new);
 
   public final BiFunction<RobotManager, Trailblazer, BaseImperativeAuto<?>> auto;
 
