33b694080a7368a81aca0cd62ff6fd0b8bd2c367
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2026-03-15 07:21:56 -0700

    decrease drive current limits

--- comp-bot/src/main/java/frc/robot/generated/CompTunerConstants.java
+++ comp-bot/src/main/java/frc/robot/generated/CompTunerConstants.java
@@ -55,7 +55,7 @@ public class CompTunerConstants {
 
     // Initial configs for the drive and steer motors and the azimuth encoder; these cannot be null.
     // Some configs will be overwritten; check the `with*InitialConfigs()` API documentation.
-    private static final TalonFXConfiguration driveInitialConfigs = new TalonFXConfiguration().withCurrentLimits(new CurrentLimitsConfigs().withSupplyCurrentLimit(Amps.of(50)));
+    private static final TalonFXConfiguration driveInitialConfigs = new TalonFXConfiguration().withCurrentLimits(new CurrentLimitsConfigs().withSupplyCurrentLimit(Amps.of(40)));
     private static final TalonFXConfiguration steerInitialConfigs = new TalonFXConfiguration()
         .withCurrentLimits(
             new CurrentLimitsConfigs()
