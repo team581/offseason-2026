084f3923c567ea474d0b1eb38327731cb2eec096
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2026-03-17 17:48:42 -0700

    get rid of SoM allusion in auto

--- comp-bot/src/main/java/frc/robot/autos/AutoSelection.java
+++ comp-bot/src/main/java/frc/robot/autos/AutoSelection.java
@@ -4,9 +4,9 @@ import com.team581.autos.AutoSelectionBase;
 import com.team581.trailblazer.Trailblazer;
 import frc.robot.autos.auto_state_machines.DoNothingAuto;
 import frc.robot.autos.auto_state_machines.LeftCircleAlternateAuto;
-import frc.robot.autos.auto_state_machines.LeftCircleSoMAuto;
+import frc.robot.autos.auto_state_machines.LeftCircleAuto;
 import frc.robot.autos.auto_state_machines.RightCircleAlternateAuto;
-import frc.robot.autos.auto_state_machines.RightCircleSoMAuto;
+import frc.robot.autos.auto_state_machines.RightCircleAuto;
 // import frc.robot.autos.auto_state_machines.RightPullSwoopShootAuto;
 import frc.robot.autos.auto_state_machines.RightStraightShootClimbAuto;
 import frc.robot.robot_manager.RobotManager;
@@ -16,9 +16,9 @@ public enum AutoSelection implements AutoSelectionBase {
   DO_NOTHING(DoNothingAuto::new),
   RIGHT_STRAIGHT_SHOOT(RightStraightShootClimbAuto::new),
   //  RIGHT_PULL_SWOOP_SHOOT(RightPullSwoopShootAuto::new),
-  RIGHT_CIRCLE_SOM(RightCircleSoMAuto::new),
+  RIGHT_CIRCLE_SOM(RightCircleAuto::new),
   RIGHT_ALTERNATE_CIRCLE_SOM(RightCircleAlternateAuto::new),
-  LEFT_CIRCLE_SOM(LeftCircleSoMAuto::new),
+  LEFT_CIRCLE_SOM(LeftCircleAuto::new),
   LEFT_ALTERNATE_CIRCLE_SOM(LeftCircleAlternateAuto::new);
 
   public final BiFunction<RobotManager, Trailblazer, BaseImperativeAuto<?>> auto;
