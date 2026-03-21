96d75a8781a96a535aba78fafd642817b48a9b43
Jonah Snider <jonah@jonahsnider.com>
2026-03-21 11:52:23 -0700

    Update shooter and deploy position constants for comp bot
    Signed-off-by: Jonah Snider <jonah@jonahsnider.com>

--- comp-bot/src/main/java/frc/robot/robot_manager/MechanismVisualizer.java
+++ comp-bot/src/main/java/frc/robot/robot_manager/MechanismVisualizer.java
@@ -3,6 +3,7 @@ package frc.robot.robot_manager;
 import dev.doglog.DogLog;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.util.Units;
@@ -14,10 +15,11 @@ public final class MechanismVisualizer {
    * pivots around.
    */
   private static final Translation3d SHOOTER_HOOD_PIVOT_POINT =
-      new Translation3d(0.118364, 0, 0.436753);
+      new Translation3d(
+          -Units.inchesToMeters(11.35), Units.inchesToMeters(13.85), Units.inchesToMeters(18.85));
 
   /** Angle from the horizontal to the deploy extension point. */
-  private static final double DEPLOY_ANGLE_FROM_HORIZONTAL = 15.327113;
+  private static final Rotation2d DEPLOY_ANGLE_FROM_HORIZONTAL = Rotation2d.fromDegrees(6.55);
 
   public static void log(
       Pose2d robotPose, double shooterHoodAngleDegrees, double deployLengthInches) {
@@ -31,7 +33,7 @@ public final class MechanismVisualizer {
     var deployPose =
         new Pose3d(
             new Translation3d(Units.inchesToMeters(deployLengthInches), 0, 0)
-                .rotateBy(new Rotation3d(0, Math.toRadians(DEPLOY_ANGLE_FROM_HORIZONTAL), 0)),
+                .rotateBy(new Rotation3d(0, DEPLOY_ANGLE_FROM_HORIZONTAL.getRadians(), 0)),
             Rotation3d.kZero);
 
     DogLog.log(
