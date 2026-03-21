44b5b06741e4e02700692af74237cb227fbb21fe
Owen <117956892+Owen756@users.noreply.github.com>
2026-03-21 11:23:12 -0700

    add climb states

--- comp-bot/src/main/java/frc/robot/robot_manager/hopper_manager/HopperState.java
+++ comp-bot/src/main/java/frc/robot/robot_manager/hopper_manager/HopperState.java
@@ -6,9 +6,24 @@ public enum HopperState {
   CLIMB,
   SCORE,
   SCORE_AND_INTAKE,
-  REHOME_DEPLOY;
+  REHOME_DEPLOY,
+
+  CLIMB_EMPTY,
+  CLIMB_APPROACH,
+  CLIMB_LINEUP,
+  CLIMB_HANG;
 
   public boolean isIntaking() {
     return this == INTAKE || this == SCORE_AND_INTAKE;
+  }
+
+  public boolean isClimbing() {
+    return switch (this) {
+      case CLIMB_EMPTY -> true;
+      case CLIMB_APPROACH -> true;
+      case CLIMB_LINEUP -> true;
+      case CLIMB_HANG -> true;
+      default -> false;
+    };
   }
 }
