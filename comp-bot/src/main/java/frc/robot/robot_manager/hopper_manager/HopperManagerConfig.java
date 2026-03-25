0b531a21c96958b1cbf164bef88d140ff36b374a
Simon Parker <87281874+simonstoryparker@users.noreply.github.com>
2026-03-25 12:25:10 -0700

    Add tunable hopper compaction delay

--- comp-bot/src/main/java/frc/robot/robot_manager/hopper_manager/HopperManagerConfig.java
+++ comp-bot/src/main/java/frc/robot/robot_manager/hopper_manager/HopperManagerConfig.java
@@ -4,4 +4,5 @@ import com.ctre.phoenix6.configs.CANrangeConfiguration;
 
 public class HopperManagerConfig {
   public static final CANrangeConfiguration CAN_RANGE_CONFIG = new CANrangeConfiguration();
+  public static final DoubleSubscriber HOPPER_COMPACTION_DELAY = DogLog.tunable("HopperManager/CompactionDelay", 0.0);
 }
