a2b55e9efa557ed8031b92023e599331c65f115d
Simon <87281874+simonp17@users.noreply.github.com>
2026-03-26 16:18:11 -0700

    Tune drive and steer gains

--- comp-bot/src/main/java/frc/robot/generated/CompTunerConstants.java
+++ comp-bot/src/main/java/frc/robot/generated/CompTunerConstants.java
@@ -24,14 +24,14 @@ public class CompTunerConstants {
     // The steer motor uses any SwerveModule.SteerRequestType control request with the
     // output type specified by SwerveModuleConstants.SteerMotorClosedLoopOutput
     private static final Slot0Configs steerGains = new Slot0Configs()
-        .withKP(100).withKI(0).withKD(0.5)
+        .withKP(290).withKI(0).withKD(0.5)
         .withKS(0.1).withKV(1.16).withKA(0)
         .withStaticFeedforwardSign(StaticFeedforwardSignValue.UseClosedLoopSign);
     // When using closed-loop control, the drive motor uses the control
     // output type specified by SwerveModuleConstants.DriveMotorClosedLoopOutput
     private static final Slot0Configs driveGains = new Slot0Configs()
-        .withKP(0.1).withKI(0).withKD(0)
-        .withKS(0).withKV(0.124);
+        .withKP(0.25).withKI(0).withKD(0)
+        .withKS(0.2).withKV(0.124);
 
     // The closed-loop output type to use for the steer motors;
     // This affects the PID/FF gains for the steer motors
