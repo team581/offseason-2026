ec7247762d7650960a0b0920c097caefd529560e
Owen <117956892+Owen756@users.noreply.github.com>
2026-03-25 18:08:49 -0700

    stop ball fill in idle_stow

--- comp-bot/src/main/java/frc/robot/robot_manager/hopper_manager/HopperState.java
+++ comp-bot/src/main/java/frc/robot/robot_manager/hopper_manager/HopperState.java
@@ -2,7 +2,7 @@ package frc.robot.robot_manager.hopper_manager;
 
 public enum HopperState {
   IDLE_DEPLOYED(true),
-  IDLE_STOWED(true),
+  IDLE_STOWED(false),
   INTAKING(true),
   EJECTING(false),
   SHOOT(false),
