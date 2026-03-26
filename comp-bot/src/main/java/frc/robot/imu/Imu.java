4e1154bc952d03329f1da9c079d6df2cdf700fe6
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2026-03-25 18:42:59 -0700

    port bumpcrossingtracker to comp bot imu

--- comp-bot/src/main/java/frc/robot/imu/Imu.java
+++ comp-bot/src/main/java/frc/robot/imu/Imu.java
@@ -2,6 +2,7 @@ package frc.robot.imu;
 
 import com.ctre.phoenix6.swerve.SwerveDrivetrain;
 import com.team581.mechanisms.imu.BaseImuSubsystem;
+import com.team581.mechanisms.imu.BumpCrossingTracker;
 import dev.doglog.DogLog;
 import edu.wpi.first.math.MathSharedStore;
 import edu.wpi.first.math.filter.LinearFilter;
@@ -15,6 +16,8 @@ public class Imu extends BaseImuSubsystem {
 
   private final LinearFilter pigeonYAccelFilter = LinearFilter.movingAverage(10);
 
+  public final BumpCrossingTracker bumpCrossingTracker;
+
   private double pigeonXPrevAccel = 0.0;
   private double pigeonYPrevAccel = 0.0;
   private double pigeonGForce = 0.0;
@@ -23,6 +26,10 @@ public class Imu extends BaseImuSubsystem {
 
   public Imu(SwerveDrivetrain<?, ?, ?> drivetrain) {
     super(SubsystemPriority.IMU, drivetrain);
+
+    this.bumpCrossingTracker =
+        new BumpCrossingTracker(
+            () -> Math.hypot(pitch, roll), () -> driveState.Pose, drivetrain::resetPose);
   }
 
   @Override
@@ -62,6 +69,7 @@ public class Imu extends BaseImuSubsystem {
     DogLog.log("Imu/Pigeon/HypotAccel", pigeonJerk);
     DogLog.log("Imu/Pigeon/GForce", pigeonGForce);
     DogLog.log("Imu/Pigeon/MaxGForce", maxGForceDetected);
+    bumpCrossingTracker.log();
   }
 
   public boolean collisionDetected() {
