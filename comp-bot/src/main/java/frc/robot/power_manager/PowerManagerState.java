2befad035faa5c99a2c1d06a219704d951af687b
Owen <117956892+Owen756@users.noreply.github.com>
2026-03-25 19:55:03 -0700

    Set current limits in power manager

--- comp-bot/src/main/java/frc/robot/power_manager/PowerManagerState.java
+++ comp-bot/src/main/java/frc/robot/power_manager/PowerManagerState.java
@@ -1,17 +1,17 @@
 package frc.robot.power_manager;
 
 public enum PowerManagerState {
-  AUTO_FIRST_SEGMENT(100, 50, 18, 10, 50, 50, 40),
-  IDLE(100, 50, 18, 10, 50, 50, 40),
-  SHOOTING(100, 50, 10, 10, 50, 50, 40);
+  AUTO_FIRST_SEGMENT(40, 50, 15, 10, 50, 50, 40),
+  IDLE(40, 50, 15, 10, 50, 50, 40),
+  SHOOTING(40, 50, 15, 10, 50, 50, 40);
 
-  final double shooterHoodSupplyCurrent;
-  final double swerveSupplyCurrent;
   final double shooterSupplyCurrent;
   final double intakeSupplyCurrent;
   final double deploySupplyCurrent;
+  final double shooterHoodSupplyCurrent;
   final double feederSupplyCurrent;
   final double conveyorSupplyCurrent;
+  final double swerveSupplyCurrent;
 
   PowerManagerState(
       double shooterSupplyCurrent,
