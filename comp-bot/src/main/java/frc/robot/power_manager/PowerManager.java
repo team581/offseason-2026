71501c1943b210fe445ce54cf2ec0e835fd463e6
Simon <87281874+simonp17@users.noreply.github.com>
2026-03-26 21:31:08 -0700

    Remove turbo mode from power manager

--- comp-bot/src/main/java/frc/robot/power_manager/PowerManager.java
+++ comp-bot/src/main/java/frc/robot/power_manager/PowerManager.java
@@ -54,10 +54,6 @@ public class PowerManager extends StateMachineSubsystem<PowerManagerState> {
     setStateFromRequest(PowerManagerState.SHOOTING);
   }
 
-  public void turboRequest() {
-    setStateFromRequest(PowerManagerState.TURBO_MODE);
-  }
-
   @Override
   protected void afterTransition(PowerManagerState newState) {
     executor.execute(
