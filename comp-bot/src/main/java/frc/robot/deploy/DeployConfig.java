81675905d5ce58515d521a3202ec6747f1b7254b
Owen <117956892+Owen756@users.noreply.github.com>
2026-03-26 17:02:03 -0700

    increase reverse torque current to 15

--- comp-bot/src/main/java/frc/robot/deploy/DeployConfig.java
+++ comp-bot/src/main/java/frc/robot/deploy/DeployConfig.java
@@ -23,7 +23,7 @@ public class DeployConfig {
 
   public static final TalonFXConfiguration MOTOR_CONFIG =
       new TalonFXConfiguration()
-          .withTorqueCurrent(new TorqueCurrentConfigs().withPeakReverseTorqueCurrent(10))
+          .withTorqueCurrent(new TorqueCurrentConfigs().withPeakReverseTorqueCurrent(15))
           .withFeedback(
               new FeedbackConfigs()
                   .withSensorToMechanismRatio((40.0 / 8.0) * (1 / (Math.PI * (2 * 0.5)))))
