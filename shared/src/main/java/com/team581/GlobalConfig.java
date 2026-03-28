13db8db83a039403ec43fc29bcb747c6ee1618c5
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2026-03-28 07:15:58 -0700

    turn off is_dev

--- shared/src/main/java/com/team581/GlobalConfig.java
+++ shared/src/main/java/com/team581/GlobalConfig.java
@@ -1,7 +1,7 @@
 package com.team581;
 
 public final class GlobalConfig {
-  public static final boolean IS_DEVELOPMENT = true;
+  public static final boolean IS_DEVELOPMENT = false;
 
   private GlobalConfig() {}
 }
