46872f7d341b5fa2012aff69c731deb1ccd79632
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2026-03-30 13:29:04 -0700

    new deploy motor on can not rio

--- comp-bot/src/main/java/frc/robot/Hardware.java
+++ comp-bot/src/main/java/frc/robot/Hardware.java
@@ -1,10 +1,15 @@
 package frc.robot;
 
 import com.ctre.phoenix6.CANBus;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.CANrange;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.mechanisms.DifferentialMotorConstants;
+import com.ctre.phoenix6.mechanisms.SimpleDifferentialMechanism;
+import com.ctre.phoenix6.signals.MotorAlignmentValue;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.deploy.DifferentialDeployConfig;
 import frc.robot.generated.CompTunerConstants;
 import frc.robot.generated.CompTunerConstants.TunerSwerveDrivetrain;
 
@@ -15,7 +20,17 @@ public class Hardware {
   private final CANBus canivore = new CANBus("581CANivore");
   private final CANBus rio = new CANBus();
 
-  public final TalonFX deployMotor = new TalonFX(15, rio);
+  public final SimpleDifferentialMechanism<TalonFX> deployDifferentialMechanism =
+      new SimpleDifferentialMechanism<>(
+          TalonFX::new,
+          new DifferentialMotorConstants<TalonFXConfiguration>()
+              .withCANBusName(canivore.getName())
+              .withLeaderId(15)
+              .withFollowerId(30)
+              .withAlignment(MotorAlignmentValue.Opposed)
+              .withLeaderInitialConfigs(DifferentialDeployConfig.LEFT_MOTOR_CONFIG)
+              .withFollowerInitialConfigs(DifferentialDeployConfig.RIGHT_MOTOR_CONFIG)
+              .withFollowerUsesCommonLeaderConfigs(true));
 
   public final TalonFX intakeLeftMotor = new TalonFX(16, rio);
   public final TalonFX intakeRightMotor = new TalonFX(17, rio);
