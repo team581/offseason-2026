cca8046eaa9e7f33db82ab617263f9bb5d2b76e4
Jordan <117786700+jordanjcoderman@users.noreply.github.com>
2026-04-02 18:53:32 -0700

    Change hopper CANrange FOV range X

--- comp-bot/src/main/java/frc/robot/robot_manager/hopper_manager/HopperManagerConfig.java
+++ comp-bot/src/main/java/frc/robot/robot_manager/hopper_manager/HopperManagerConfig.java
@@ -10,7 +10,7 @@ import edu.wpi.first.networktables.DoubleSubscriber;
 public class HopperManagerConfig {
   public static final CANrangeConfiguration CAN_RANGE_CONFIG =
       new CANrangeConfiguration()
-          .withFovParams(new FovParamsConfigs().withFOVRangeX(10.0).withFOVRangeY(10.0))
+          .withFovParams(new FovParamsConfigs().withFOVRangeX(-10.0).withFOVRangeY(10.0))
           .withToFParams(new ToFParamsConfigs().withUpdateMode(UpdateModeValue.ShortRange100Hz));
   public static final DoubleSubscriber HOPPER_COMPACTION_DELAY =
       DogLog.tunable("HopperManager/CompactionDelay", 0.2);
