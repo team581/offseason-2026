93fde1db5e1b53e6a53eabf6b03e7eda5c6fd33a
Jonah Snider <jonah@jonahsnider.com>
2026-04-02 20:34:47 -0700

    Fix deploy homing fetching stator current multiple times

--- comp-bot/src/main/java/frc/robot/deploy/Deploy.java
+++ comp-bot/src/main/java/frc/robot/deploy/Deploy.java
@@ -83,8 +83,8 @@ public class Deploy extends StateMachineSubsystem<DeployState> implements PowerM
       case UNHOMED, INTAKE, STOW -> currentState;
 
       case HOME_INWARD -> {
-        if (leftMotor.getStatorCurrent().getValueAsDouble() > DeployConfig.HOMING_CURRENT
-            && rightMotor.getStatorCurrent().getValueAsDouble() > DeployConfig.HOMING_CURRENT) {
+        if (leftStatorCurrent > DeployConfig.HOMING_CURRENT
+            && rightStatorCurrent > DeployConfig.HOMING_CURRENT) {
           differentialMechanism.setPosition(DeployConfig.HOMING_END_POSITION_INWARD);
           yield DeployState.INTAKE;
         } else {
@@ -93,8 +93,8 @@ public class Deploy extends StateMachineSubsystem<DeployState> implements PowerM
       }
 
       case HOME_OUTWARD -> {
-        if (leftMotor.getStatorCurrent().getValueAsDouble() > DeployConfig.HOMING_CURRENT
-            && rightMotor.getStatorCurrent().getValueAsDouble() > DeployConfig.HOMING_CURRENT) {
+        if (leftStatorCurrent > DeployConfig.HOMING_CURRENT
+            && rightStatorCurrent > DeployConfig.HOMING_CURRENT) {
           differentialMechanism.setPosition(DeployConfig.HOMING_END_POSITION_OUTWARD);
           yield DeployState.INTAKE;
         } else {
