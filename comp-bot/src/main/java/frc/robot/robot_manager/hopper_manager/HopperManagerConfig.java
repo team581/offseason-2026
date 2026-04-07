1899258e6730b07106090c95ed060342dd9ea340
Simon <87281874+simonp17@users.noreply.github.com>
2026-04-07 14:30:07 -0700

    Move canrange FOV center downward

--- comp-bot/src/main/java/frc/robot/robot_manager/hopper_manager/HopperManagerConfig.java
+++ comp-bot/src/main/java/frc/robot/robot_manager/hopper_manager/HopperManagerConfig.java
@@ -10,7 +10,8 @@ import edu.wpi.first.networktables.DoubleSubscriber;
 public class HopperManagerConfig {
   public static final CANrangeConfiguration CAN_RANGE_CONFIG =
       new CANrangeConfiguration()
-          .withFovParams(new FovParamsConfigs().withFOVRangeX(-10.0).withFOVRangeY(10.0))
+          .withFovParams(
+              new FovParamsConfigs().withFOVRangeX(10.0).withFOVRangeY(10.0).withFOVCenterY(-11.8))
           .withToFParams(new ToFParamsConfigs().withUpdateMode(UpdateModeValue.ShortRange100Hz));
   public static final DoubleSubscriber HOPPER_COMPACTION_DELAY =
       DogLog.tunable("HopperManager/CompactionDelay", 0.7);
