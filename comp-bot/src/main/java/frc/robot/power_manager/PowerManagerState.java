38d3ea7ec4ed7f46fe1342ee04fb1003170bf407
Simon <87281874+simonp17@users.noreply.github.com>
2026-04-08 13:26:15 -0700

    Lower swerve limits when scoring

--- comp-bot/src/main/java/frc/robot/power_manager/PowerManagerState.java
+++ comp-bot/src/main/java/frc/robot/power_manager/PowerManagerState.java
@@ -3,8 +3,8 @@ package frc.robot.power_manager;
 public enum PowerManagerState {
   AUTO_FIRST_SEGMENT(25, 40, 15, 40, 10, 10, 60),
   IDLE(10, 25, 15, 40, 10, 10, 40),
-  SCORING(25, 20, 15, 40, 20, 20, 40),
-  SCORING_FAR(25, 20, 15, 40, 10, 10, 40),
+  SCORING(25, 20, 15, 40, 20, 20, 20),
+  SCORING_FAR(25, 20, 15, 40, 10, 10, 20),
   FEEDING(35, 20, 15, 40, 15, 15, 40),
   TURBO_MODE(25, 5, 18, 10, 10, 10, 60);
 
