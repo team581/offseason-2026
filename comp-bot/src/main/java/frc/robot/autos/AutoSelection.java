a33a47e33d880bdbe79a84baf3a3d3fcb878390e
Jonah Snider <jonah@jonahsnider.com>
2026-04-08 20:53:25 -0700

    Remove unused autos from comp-bot
    Signed-off-by: Jonah Snider <jonah@jonahsnider.com>

--- comp-bot/src/main/java/frc/robot/autos/AutoSelection.java
+++ comp-bot/src/main/java/frc/robot/autos/AutoSelection.java
@@ -5,7 +5,6 @@ import com.team581.trailblazer.Trailblazer;
 import frc.robot.autos.auto_state_machines.DoNothingAuto;
 import frc.robot.autos.auto_state_machines.LeftIntegratedAuto;
 import frc.robot.autos.auto_state_machines.RightIntegratedAuto;
-// import frc.robot.autos.auto_state_machines.RightPullSwoopShootAuto;
 import frc.robot.robot_manager.RobotManager;
 import java.util.function.BiFunction;
 
@@ -13,12 +12,6 @@ public enum AutoSelection implements AutoSelectionBase {
   DO_NOTHING(DoNothingAuto::new),
   RIGHT_INTEGRATED_AUTO(RightIntegratedAuto::new),
   LEFT_INTEGRATED_AUTO(LeftIntegratedAuto::new);
-  // RIGHT_STRAIGHT_SHOOT(RightStraightShootClimbAuto::new),
-  //  RIGHT_PULL_SWOOP_SHOOT(RightPullSwoopShootAuto::new),
-  // RIGHT_CIRCLE_SOM(RightCircleAuto::new),
-  // RIGHT_ALTERNATE_CIRCLE_SOM(RightCircleAlternateAuto::new),
-  // LEFT_CIRCLE_SOM(LeftCircleAuto::new),
-  // LEFT_ALTERNATE_CIRCLE_SOM(LeftCircleAlternateAuto::new);
 
   public final BiFunction<RobotManager, Trailblazer, BaseImperativeAuto<?>> auto;
 
