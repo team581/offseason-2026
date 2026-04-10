04e24d855818a683ad4e787a023861de10f8e90c
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2026-04-10 13:43:36 -0700

    brake mode for deploy

--- comp-bot/src/main/java/frc/robot/deploy/DeployConfig.java
+++ comp-bot/src/main/java/frc/robot/deploy/DeployConfig.java
@@ -42,7 +42,7 @@ public class DeployConfig {
                   .withSensorToMechanismRatio((40.0 / 8.0) * (1 / (Math.PI * (2 * 0.5)))))
           .withMotorOutput(
               new MotorOutputConfigs()
-                  .withNeutralMode(NeutralModeValue.Coast)
+                  .withNeutralMode(NeutralModeValue.Brake)
                   .withInverted(InvertedValue.Clockwise_Positive))
           .withCurrentLimits(
               new CurrentLimitsConfigs().withStatorCurrentLimit(50).withSupplyCurrentLimit(18))
@@ -64,7 +64,7 @@ public class DeployConfig {
                   .withSensorToMechanismRatio((40.0 / 8.0) * (1 / (Math.PI * (2 * 0.5)))))
           .withMotorOutput(
               new MotorOutputConfigs()
-                  .withNeutralMode(NeutralModeValue.Coast)
+                  .withNeutralMode(NeutralModeValue.Brake)
                   .withInverted(InvertedValue.CounterClockwise_Positive))
           .withCurrentLimits(
               new CurrentLimitsConfigs().withStatorCurrentLimit(50).withSupplyCurrentLimit(18))
