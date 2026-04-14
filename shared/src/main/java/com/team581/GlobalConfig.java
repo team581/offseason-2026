ca67b92fcc6d45430ddcde6f74db7ce5b6f7b6ab
Jonah Snider <jonah@jonahsnider.com>
2026-04-14 16:36:20 -0700

    Turn on clamped auto points and development mode
    Signed-off-by: Jonah Snider <jonah@jonahsnider.com>

--- shared/src/main/java/com/team581/GlobalConfig.java
+++ shared/src/main/java/com/team581/GlobalConfig.java
@@ -1,7 +1,7 @@
 package com.team581;
 
 public final class GlobalConfig {
-  public static final boolean IS_DEVELOPMENT = false;
+  public static final boolean IS_DEVELOPMENT = true;
 
   private GlobalConfig() {}
 }
