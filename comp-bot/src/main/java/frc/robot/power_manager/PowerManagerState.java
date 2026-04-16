63c6a6f26eb6183c902a9b1ca6fc157a68db515c
Simon <87281874+simonp17@users.noreply.github.com>
2026-04-16 14:52:29 -0700

    Lower shooter supply current limit when idle

--- comp-bot/src/main/java/frc/robot/power_manager/PowerManagerState.java
+++ comp-bot/src/main/java/frc/robot/power_manager/PowerManagerState.java
@@ -2,7 +2,7 @@ package frc.robot.power_manager;
 
 public enum PowerManagerState {
   AUTO_FIRST_SEGMENT(30, 40, 15, 40, 10, 10, 70),
-  IDLE(10, 25, 15, 40, 10, 10, 45),
+  IDLE(5, 25, 15, 40, 10, 10, 45),
   SCORING(50, 20, 15, 40, 20, 20, 20),
   SCORING_FAR(50, 20, 15, 40, 10, 10, 20),
   SUPER_SCORING(50, 10, 15, 40, 30, 30, 20),
