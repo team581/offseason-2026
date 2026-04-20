45ad097f46a0b8a0602709d65d1388dc6afee0c0
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2026-04-19 21:51:30 -0700

    right and left special autos

--- comp-bot/src/main/java/frc/robot/autos/AutoSelection.java
+++ comp-bot/src/main/java/frc/robot/autos/AutoSelection.java
@@ -4,14 +4,18 @@ import com.team581.autos.AutoSelectionBase;
 import com.team581.trailblazer.Trailblazer;
 import frc.robot.autos.auto_state_machines.DoNothingAuto;
 import frc.robot.autos.auto_state_machines.LeftIntegratedAuto;
+import frc.robot.autos.auto_state_machines.LeftSpecialAuto;
 import frc.robot.autos.auto_state_machines.RightIntegratedAuto;
+import frc.robot.autos.auto_state_machines.RightSpecialAuto;
 import frc.robot.robot_manager.RobotManager;
 import java.util.function.BiFunction;
 
 public enum AutoSelection implements AutoSelectionBase {
   DO_NOTHING(DoNothingAuto::new),
   RIGHT_INTEGRATED_AUTO(RightIntegratedAuto::new),
-  LEFT_INTEGRATED_AUTO(LeftIntegratedAuto::new);
+  RIGHT_SPECIAL_AUTO(RightSpecialAuto::new),
+  LEFT_INTEGRATED_AUTO(LeftIntegratedAuto::new),
+  LEFT_SPECIAL_AUTO(LeftSpecialAuto::new);
 
   public final BiFunction<RobotManager, Trailblazer, BaseImperativeAuto<?>> auto;
 
