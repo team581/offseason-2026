497070bdcd7b3b7f1ae8a2016965ce1f6d000b6d
Simon <87281874+simonp17@users.noreply.github.com>
2026-04-21 20:15:40 -0700

    Lower debounce time for bump crossing flat

--- shared/src/main/java/com/team581/mechanisms/imu/BumpCrossingTracker.java
+++ shared/src/main/java/com/team581/mechanisms/imu/BumpCrossingTracker.java
@@ -13,7 +13,7 @@ import java.util.function.Consumer;
 import java.util.function.DoubleSupplier;
 
 public class BumpCrossingTracker extends StateMachine<BumpCrossingState> {
-  private static final double FLAT_DEBOUNCE_SECONDS = 0.1;
+  private static final double FLAT_DEBOUNCE_SECONDS = 0.04;
   private static final double FLAT_FALLBACK_DEBOUNCE_SECONDS = 0.75;
   private static final DoubleSubscriber FLAT_THRESHOLD =
       DogLog.tunable("BumpCrossing/FlatThresholdDegrees", 3.0);
