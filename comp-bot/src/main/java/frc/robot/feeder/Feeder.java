4ac52cf4fc67a393d6490126988def2e1c7045cb
Jonah Snider <jonah@jonahsnider.com>
2026-04-24 09:02:29 -0700

    Use batch status signal refresh in feeder
    Signed-off-by: Jonah Snider <jonah@jonahsnider.com>

--- comp-bot/src/main/java/frc/robot/feeder/Feeder.java
+++ comp-bot/src/main/java/frc/robot/feeder/Feeder.java
@@ -1,5 +1,7 @@
 package frc.robot.feeder;
 
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.controls.NeutralOut;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
@@ -7,7 +9,10 @@ import com.team581.math.MathHelpers;
 import com.team581.mechanisms.PowerManaged;
 import com.team581.util.state_machines.StateMachineSubsystem;
 import dev.doglog.DogLog;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Current;
 import frc.robot.util.scheduling.SubsystemPriority;
+import java.util.List;
 
 public class Feeder extends StateMachineSubsystem<FeederState> implements PowerManaged {
 
@@ -15,6 +20,12 @@ public class Feeder extends StateMachineSubsystem<FeederState> implements PowerM
   private final TalonFX bottomMotor;
   private final NeutralOut neutralRequest = new NeutralOut();
   private final VoltageOut voltageRequest = new VoltageOut(0).withEnableFOC(true);
+
+  private final StatusSignal<AngularVelocity> topVelocitySignal;
+  private final StatusSignal<AngularVelocity> bottomVelocitySignal;
+  private final StatusSignal<Current> topStatorCurrentSignal;
+  private final StatusSignal<Current> bottomStatorCurrentSignal;
+  private final List<BaseStatusSignal> allSignals;
 
   private double averageCurrent = 0.0;
 
@@ -24,6 +35,17 @@ public class Feeder extends StateMachineSubsystem<FeederState> implements PowerM
     bottomMotor.getConfigurator().apply(FeederConfig.BOTTOM_MOTOR_CONFIG);
     this.topMotor = topMotor;
     this.bottomMotor = bottomMotor;
+
+    topVelocitySignal = topMotor.getVelocity(false);
+    bottomVelocitySignal = bottomMotor.getVelocity(false);
+    topStatorCurrentSignal = topMotor.getStatorCurrent(false);
+    bottomStatorCurrentSignal = bottomMotor.getStatorCurrent(false);
+    allSignals =
+        List.of(
+            topVelocitySignal,
+            bottomVelocitySignal,
+            topStatorCurrentSignal,
+            bottomStatorCurrentSignal);
   }
 
   public void shootRequest() {
@@ -62,14 +84,15 @@ public class Feeder extends StateMachineSubsystem<FeederState> implements PowerM
 
   @Override
   protected void collectInputs() {
-    DogLog.log("Feeder/Top/VelocityRPM", topMotor.getVelocity().getValueAsDouble() * 60.0);
-    DogLog.log("Feeder/Bottom/VelocityRPM", bottomMotor.getVelocity().getValueAsDouble() * 60.0);
+    BaseStatusSignal.refreshAll(allSignals);
+    DogLog.log("Feeder/Top/VelocityRPM", topVelocitySignal.getValueAsDouble() * 60.0);
+    DogLog.log("Feeder/Bottom/VelocityRPM", bottomVelocitySignal.getValueAsDouble() * 60.0);
     DogLog.log("Feeder/Voltage", getState().getVoltage());
 
     averageCurrent =
         MathHelpers.average(
-            topMotor.getStatorCurrent().getValueAsDouble(),
-            bottomMotor.getStatorCurrent().getValueAsDouble());
+            topStatorCurrentSignal.getValueAsDouble(),
+            bottomStatorCurrentSignal.getValueAsDouble());
   }
 
   public double getAverageCurrent() {
