aeb2f4d78796be16d865c2dde82a3b1968f15ed9
Simon <87281874+simonp17@users.noreply.github.com>
2026-04-26 13:21:59 -0700

    Turn off bump assist by default

--- comp-bot/src/main/java/frc/robot/config/DSOptions.java
+++ comp-bot/src/main/java/frc/robot/config/DSOptions.java
@@ -6,7 +6,7 @@ import edu.wpi.first.networktables.BooleanSubscriber;
 public final class DSOptions {
   public static final BooleanSubscriber USE_TAG_LIMELIGHTS = DSOption.of("UseTagLimelights", true);
   public static final BooleanSubscriber USE_HUB_STATE = DSOption.of("UseHubState", true);
-  public static final BooleanSubscriber USE_BUMP_ASSIST = DSOption.of("UseBumpAssist", true);
+  public static final BooleanSubscriber USE_BUMP_ASSIST = DSOption.of("UseBumpAssist", false);
   public static final BooleanSubscriber USE_CANRANGE = DSOption.of("UseCANRange", true);
   public static final BooleanSubscriber USE_TOWER_SENSOR = DSOption.of("UseTowerSensor", true);
 
