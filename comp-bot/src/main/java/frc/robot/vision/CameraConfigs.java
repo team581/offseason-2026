ed4dd5906d7a86e7fa155c1472ab1e27a984663f
Simon <87281874+simonp17@users.noreply.github.com>
2026-04-27 11:44:17 -0700

    Make sure ground limelight has updated angle

--- comp-bot/src/main/java/frc/robot/vision/CameraConfigs.java
+++ comp-bot/src/main/java/frc/robot/vision/CameraConfigs.java
@@ -68,7 +68,7 @@ public class CameraConfigs {
           Units.inchesToMeters(25.054733),
           Units.inchesToMeters(0.0),
           Units.inchesToMeters(12.056034),
-          -20.42,
+          -21.4,
           0.0,
           0.0);
 
