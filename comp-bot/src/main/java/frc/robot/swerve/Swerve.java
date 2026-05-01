5bef503187865aedf9dc79acc07b48773bf9f388
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2026-05-01 07:59:10 -0500

    Fix swerve not pointed straight fault

--- comp-bot/src/main/java/frc/robot/swerve/Swerve.java
+++ comp-bot/src/main/java/frc/robot/swerve/Swerve.java
@@ -603,7 +603,9 @@ public class Swerve extends StateMachineSubsystem<SwerveState> implements PowerM
         var actual = current[i];
         var target = targets[i];
 
-        if (MathUtil.isNear(actual.angle.getDegrees(), target.angle.getDegrees(), 10, -180, 180)) {
+        if (MathUtil.isNear(actual.angle.getDegrees(), target.angle.getDegrees(), 10, -180, 180)
+            || MathUtil.isNear(
+                actual.angle.getDegrees(), target.angle.getDegrees() + 180, 10, -180, 180)) {
           // it's within tolerance
         } else {
           isMisaligned = true;
