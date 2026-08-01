926cad0ea6f832b58bb30f3a93b057afb52a7d9c
rhetorr <140564619+rhetorr@users.noreply.github.com>
2026-08-01 12:02:14 -0700

    Control intake with buttons.

--- new-member-bot/src/main/java/frc/robot/Robot.java
+++ new-member-bot/src/main/java/frc/robot/Robot.java
@@ -1,9 +1,14 @@
 package frc.robot;
 
+import com.ctre.phoenix6.hardware.TalonFX;
 import com.team581.Base581Robot;
+import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.generated.BuildConstants;
 
 public class Robot extends Base581Robot {
+  private XboxController xbox = new XboxController(0);
+  private TalonFX intakeMotor = new TalonFX(16);
+
   public Robot() {
     logMetadata(
         BuildConstants.MAVEN_NAME,
@@ -14,6 +19,18 @@ public class Robot extends Base581Robot {
         BuildConstants.DIRTY);
 
     finalizeInit();
+  }
+
+  @Override
+  public void robotPeriodic() {
+    super.robotPeriodic();
+    if (xbox.getXButton()) {
+      intakeMotor.setVoltage(3.0);
+    } else if (xbox.getYButton()) {
+      intakeMotor.setVoltage(-2.0);
+    } else if (xbox.getAButton()) {
+      intakeMotor.setVoltage(0.0);
+    }
   }
 
   @Override
