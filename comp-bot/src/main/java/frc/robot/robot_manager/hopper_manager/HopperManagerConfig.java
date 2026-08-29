4f9244c19c08f8d16921a15da863de256f791dde
Simon <87281874+simonp17@users.noreply.github.com>
2026-08-29 11:58:48 -0700

    Shuffle deploy during beast mode

--- comp-bot/src/main/java/frc/robot/robot_manager/hopper_manager/HopperManagerConfig.java
+++ comp-bot/src/main/java/frc/robot/robot_manager/hopper_manager/HopperManagerConfig.java
@@ -21,4 +21,6 @@ public class HopperManagerConfig {
       DogLog.tunable("HopperManager/CompactionDelay", 0.25);
   public static final DoubleSubscriber HOPPER_COMPACTION_SHUFFLE_INTERVAL =
       DogLog.tunable("HopperManager/CompactionShuffleInterval", 0.25);
+  public static final DoubleSubscriber BEAST_MODE_SHUFFLE_INTERVAL =
+      DogLog.tunable("HopperManager/BeastModeShuffleInterval", 0.125);
 }
