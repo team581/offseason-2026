72534067e21f6bdb464aa4b66148d45399d7ed15
Simon <87281874+simonstoryparker@users.noreply.github.com>
2026-09-03 10:44:41 -0700

    Fix robot.java typos in offseason bot causing formatter not to run

--- offseason-bot/src/main/java/frc/robot/Robot.java
+++ offseason-bot/src/main/java/frc/robot/Robot.java
@@ -22,40 +22,81 @@ import frc.robot.turret.Turret;
 import frc.robot.vision.Vision;
 
 public class Robot extends Base581Robot {
-	private final Hardware hardware = new Hardware();
+  private final Hardware hardware = new Hardware();
 
-	private final Imu imu = new Imu(hardware.drivetrain);
-	private final Trailblazer trailblazer = new Trailblazer(new HeuristicPathTracker(new PoseErrorTolerance(0.5, 10)),
-			new BumpCrossingFollower(new PidPathFollower(new PIDController(3.5, 0, 0),
-					new PIDController(Swerve.ORIGINAL_HEADING_PID.getP(), Swerve.ORIGINAL_HEADING_PID.getI(),
-							Swerve.ORIGINAL_HEADING_PID.getD())),
-					imu.bumpCrossingTracker));
+  private final Imu imu = new Imu(hardware.drivetrain);
+  private final Trailblazer trailblazer =
+      new Trailblazer(
+          new HeuristicPathTracker(new PoseErrorTolerance(0.5, 10)),
+          new BumpCrossingFollower(
+              new PidPathFollower(
+                  new PIDController(3.5, 0, 0),
+                  new PIDController(
+                      Swerve.ORIGINAL_HEADING_PID.getP(),
+                      Swerve.ORIGINAL_HEADING_PID.getI(),
+                      Swerve.ORIGINAL_HEADING_PID.getD())),
+              imu.bumpCrossingTracker));
 
-	private final HealthManager health = new HealthManager(hardware.shooterLimeLight, hardware.leftLimeLight,
-			hardware.rightLimeLight, hardware.groundLimeLight);
-	private final Vision vision = new Vision(imu, hardware.shooterLimeLight, hardware.leftLimeLight,
-			hardware.rightLimeLight, hardware.groundLimeLight);
-	private final Swerve swerve = new Swerve(hardware.drivetrain, health, hardware.driverController, trailblazer);
-	private final Localization localization = new Localization(swerve, hardware.drivetrain, vision, imu);
+  private final HealthManager health =
+      new HealthManager(
+          hardware.shooterLimeLight,
+          hardware.leftLimeLight,
+          hardware.rightLimeLight,
+          hardware.groundLimeLight);
+  private final Vision vision =
+      new Vision(
+          imu,
+          hardware.shooterLimeLight,
+          hardware.leftLimeLight,
+          hardware.rightLimeLight,
+          hardware.groundLimeLight);
+  private final Swerve swerve =
+      new Swerve(hardware.drivetrain, health, hardware.driverController, trailblazer);
+  private final Localization localization =
+      new Localization(swerve, hardware.drivetrain, vision, imu);
 
-	private final ShooterHood shooterHood = new ShooterHood(hardware.shooterHoodMotor);
-	private final Shooter shooter = new Shooter(hardware.shooterTopLeftMotor, hardware.shooterTopRightMotor,
-			hardware.shooterBottomLeftMotor, hardware.shooterBottomRightMotor);
+  private final ShooterHood shooterHood = new ShooterHood(hardware.shooterHoodMotor);
+  private final Shooter shooter =
+      new Shooter(
+          hardware.shooterTopLeftMotor,
+          hardware.shooterTopRightMotor,
+          hardware.shooterBottomLeftMotor,
+          hardware.shooterBottomRightMotor);
 
-	private final HubActivity hubActivity = new HubActivity();
-	private final Turret turret = new Turret(hardware.turretMotor, hardware.turretEncoder, vision);
-	private final PowerManager powerManager = new PowerManager(shooter, null, null, shooterHood, null, null, swerve);
-	private final RobotManager robotManager = new RobotManager(null, shooterHood, localization, swerve, shooter, vision,
-			hardware.driverController, health, hubActivity, trailblazer, null, hardware, powerManager, turret);
+  private final HubActivity hubActivity = new HubActivity();
+  private final Turret turret = new Turret(hardware.turretMotor, hardware.turretEncoder, vision);
+  private final PowerManager powerManager =
+      new PowerManager(shooter, null, null, shooterHood, null, null, swerve);
+  private final RobotManager robotManager =
+      new RobotManager(
+          null,
+          shooterHood,
+          localization,
+          swerve,
+          shooter,
+          vision,
+          hardware.driverController,
+          health,
+          hubActivity,
+          trailblazer,
+          null,
+          hardware,
+          powerManager,
+          turret);
 
-	public Robot() {
-		logMetadata(BuildConstants.MAVEN_NAME, BuildConstants.BUILD_DATE, BuildConstants.GIT_SHA,
-				BuildConstants.GIT_DATE, BuildConstants.GIT_BRANCH, BuildConstants.DIRTY);
+  public Robot() {
+    logMetadata(
+        BuildConstants.MAVEN_NAME,
+        BuildConstants.BUILD_DATE,
+        BuildConstants.GIT_SHA,
+        BuildConstants.GIT_DATE,
+        BuildConstants.GIT_BRANCH,
+        BuildConstants.DIRTY);
 
-		finalizeInit();
-	}
+    finalizeInit();
+  }
 
-	@Override
+  @Override
   protected void configureBindings() {
     var driver =
         new ControllerBindings(buttonBindingsLoop, enabledEvent, hardware.driverController);
@@ -76,13 +117,16 @@ public class Robot extends Base581Robot {
 
     driver.rightBumper().onPress(robotManager::stowDeployRequest);
 
-    driver.leftBumper().onPress(()->hopperManager.setDriverWantsEject(true)).onRelease(()->hopperManager.setDriverWantsEject(false));
+    driver
+        .leftBumper()
+        .onPress(() -> hopperManager.setDriverWantsEject(true))
+        .onRelease(() -> hopperManager.setDriverWantsEject(false));
 
     operator.leftTrigger().onPress(robotManager::stowDeployRequest);
     operator.rightTrigger().onPress(robotManager::prepareScoreRequest);
 
-    operator.x().onPress(robotManager::unjamRequest).onRelease(robotManager::idleRequest)
-    operator.y().onPress(powerManager::turboRequest)
+    operator.x().onPress(robotManager::unjamRequest).onRelease(robotManager::idleRequest);
+    operator.y().onPress(powerManager::turboRequest);
 
     operator.leftBumper().onPress(powerManager::prioritizeIntakeRequest);
     operator
